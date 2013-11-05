1aabde5fdfbd7c335f777a3be8dbc110491d3768
edeustace <ed.eustace@gmail.com>
2013-11-05 14:36:50 +0100

    reset choices before updating the ui

--- components/corespring/multiple-choice/src/client/render.js
+++ components/corespring/multiple-choice/src/client/render.js
@@ -1,158 +1,168 @@
-var link, main;
 
-link = function($sce) {
-  return function(scope, element, attrs) {
+var main = [
+  '$sce', '$log',
 
-    scope.inputType = 'checkbox';
-
-    scope.answer = {
-      choices: {}
-    };
-
-    var getAnswers = function(){
-      if (scope.answer.choice) {
-        return [scope.answer.choice];
-      } else {
-        var isTrue = function(k){ return scope.answer.choices[k] === true;};
-        var allKeys = _.keys(scope.answer.choices);
-        var keys = _.filter(allKeys, isTrue);
-        return keys;
-      }
-    };
-
-    var applyChoices = function(){
-
-      if(!scope.question || !scope.session.answers){
-        return;
-      }
-
-      var answers = scope.session.answers;
-
-      if( scope.inputType == "radio"){
-        scope.answer.choice = answers[0];
-      } else {
-        for (var i = 0; i < answers.length; i++) {
-          var key = answers[i];
-          scope.answer.choices[key] = true;
-        }
-      }
-    };
-
-    //TODO: Reset a function exposed in the bridge?
-    var resetFeedback = function(choices){
-      console.log("choices: ", choices);
-      _.each(choices, function(c){
-        if (c) {
-          delete c.feedback;
-          delete c.correct;
-        };
-      });
-    };
-
-    var layoutChoices = function(choices, order){
-      if(!order){
-        var shuffled = _.shuffle(_.cloneDeep(choices));
-        return shuffled;
-      } else {
-        var ordered = _.map(order, function(v){
-          return _.find(choices, function(c){
-            return c.value == v;
-          });
-        });
-
-        var missing = _.difference(choices, ordered)
-        return _.union(ordered, missing);
-      }
-    };
-
-    var stashOrder = function(choices) {
-      return _.map(choices, function(c){
-        return c.value;
-      });
-    };
-
-    var updateUi = function(){
-
-      if(!scope.question || !scope.session) {
-        return;
-      }
-
-      var model = scope.question;
-      var stash = scope.session.stash = scope.session.stash || {};
-      var answers = scope.session.answers = scope.session.answers || {};
-
-      scope.inputType = !!model.config.singleChoice ? "radio" : "checkbox";
-
-      if(stash.shuffledOrder && model.config.shuffle){
-        scope.choices = layoutChoices(model.choices, stash.shuffledOrder)
-      } else if(model.config.shuffle) {
-        scope.choices = layoutChoices(model.choices)
-        stash.shuffledOrder = stashOrder(scope.choices);
-        scope.$emit('saveStash', attrs.id, stash);
-      } else {
-        scope.choices = _.cloneDeep(scope.question.choices);
-      }
-
-      applyChoices();
-    };
-
-    scope.containerBridge = {
-
-      setDataAndSession: function(dataAndSession) {
-        scope.question = dataAndSession.data.model;
-        scope.session = dataAndSession.session || {};
-        updateUi();
-      },
-
-      getSession: function() {
-        return {
-          answers: getAnswers(),
-          stash: scope.session.stash
-        };
-      },
-
-      // sets the server's response
-      setResponse: function(response) {
-        console.log( scope.$id, "set response for multiple-choice", response);
-        console.log( scope.$id, "choices", scope.choices);
-
-        resetFeedback(scope.choices);
-
-        scope.response = response;
-        if (response.feedback) {
-          _.each(response.feedback, function(fb) {
-
-            var choice = _.find(scope.choices, function(c) {
-              return c.value === fb.value;
-            });
-
-            if (choice !== null) {
-              choice.feedback = fb.feedback;
-              choice.correct = fb.correct;
-            }
-            console.log("choice: ", choice);
-          });
-        }
-      },
-
-      setGlobalSession: function(session){
-        scope.sessionFinished = session.isFinished;
-      }
-    };
-
-    scope.$emit('registerComponent', attrs.id, scope.containerBridge);
-  };
-};
-
-main = [
-  '$sce',
-  function($sce) {
+  function($sce, $log) {
     var def;
+
+    var link = function(scope, element, attrs) {
+
+      scope.inputType = 'checkbox';
+
+      scope.answer = {
+        choices: {}
+      };
+
+      var getAnswers = function(){
+        if (scope.answer.choice) {
+          return [scope.answer.choice];
+        } else {
+          var isTrue = function(k){ return scope.answer.choices[k] === true;};
+          var allKeys = _.keys(scope.answer.choices);
+          var keys = _.filter(allKeys, isTrue);
+          return keys;
+        }
+      };
+
+      var applyChoices = function(){
+
+        if(!scope.question || !scope.session.answers){
+          return;
+        }
+
+        var answers = scope.session.answers;
+
+        if( scope.inputType == "radio"){
+          scope.answer.choice = answers[0];
+        } else {
+          for (var i = 0; i < answers.length; i++) {
+            var key = answers[i];
+            scope.answer.choices[key] = true;
+          }
+        }
+      };
+
+      //TODO: Reset a function exposed in the bridge?
+      var resetFeedback = function(choices){
+        console.log("choices: ", choices);
+        _.each(choices, function(c){
+          if (c) {
+            delete c.feedback;
+            delete c.correct;
+          };
+        });
+      };
+
+      var resetChoices = function(){
+        scope.answer.choices = [];
+      };
+
+      var layoutChoices = function(choices, order){
+        if(!order){
+          var shuffled = _.shuffle(_.cloneDeep(choices));
+          return shuffled;
+        } else {
+          var ordered = _.map(order, function(v){
+            return _.find(choices, function(c){
+              return c.value == v;
+            });
+          });
+
+          var missing = _.difference(choices, ordered)
+          return _.union(ordered, missing);
+        }
+      };
+
+      var stashOrder = function(choices) {
+        return _.map(choices, function(c){
+          return c.value;
+        });
+      };
+
+      var updateUi = function(){
+
+        if(!scope.question || !scope.session) {
+          return;
+        }
+
+        resetChoices();
+
+        var model = scope.question;
+        var stash = scope.session.stash = scope.session.stash || {};
+        var answers = scope.session.answers = scope.session.answers || {};
+
+        scope.inputType = !!model.config.singleChoice ? "radio" : "checkbox";
+
+        if(stash.shuffledOrder && model.config.shuffle){
+          scope.choices = layoutChoices(model.choices, stash.shuffledOrder)
+        } else if(model.config.shuffle) {
+          scope.choices = layoutChoices(model.choices)
+          stash.shuffledOrder = stashOrder(scope.choices);
+          scope.$emit('saveStash', attrs.id, stash);
+        } else {
+          scope.choices = _.cloneDeep(scope.question.choices);
+        }
+
+        applyChoices();
+      };
+
+      scope.containerBridge = {
+
+        setDataAndSession: function(dataAndSession) {
+          $log.debug("multiple-choice setDataAndSession", dataAndSession);
+          scope.question = dataAndSession.data.model;
+          scope.session = dataAndSession.session || {};
+          updateUi();
+        },
+
+        getSession: function() {
+
+          var stash = (scope.session && scope.session.stash) ? scope.session.stash : {};
+
+          return {
+            answers: getAnswers(),
+            stash: stash
+          };
+        },
+
+        // sets the server's response
+        setResponse: function(response) {
+          console.log( scope.$id, "set response for multiple-choice", response);
+          console.log( scope.$id, "choices", scope.choices);
+
+          resetFeedback(scope.choices);
+
+          scope.response = response;
+          if (response.feedback) {
+            _.each(response.feedback, function(fb) {
+
+              var choice = _.find(scope.choices, function(c) {
+                return c.value === fb.value;
+              });
+
+              if (choice !== null) {
+                choice.feedback = fb.feedback;
+                choice.correct = fb.correct;
+              }
+              console.log("choice: ", choice);
+            });
+          }
+        },
+
+        setGlobalSession: function(session){
+          scope.sessionFinished = session.isFinished;
+        }
+      };
+
+      scope.$emit('registerComponent', attrs.id, scope.containerBridge);
+    };
+
+
     def = {
       scope: {},
       restrict: 'E',
       replace: true,
-      link: link($sce),
+      link: link,
       template: [ '<div class="view-multiple-choice">',
                   '  <label ng-bind-html-unsafe="question.prompt"></label>',
                   '  <div class="choices-container" ng-class="question.config.orientation">',
@@ -171,7 +181,6 @@ main = [
                   '</div>',
                   '</div>'].join("\n")
     };
-
 
     return def;
   }
