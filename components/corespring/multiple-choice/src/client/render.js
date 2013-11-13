49aeea846a33a3aedd945249107b83e964b95d9c
edeustace <ed.eustace@gmail.com>
2013-11-13 14:49:41 +0000

    remove test directive

--- components/corespring/multiple-choice/src/client/render.js
+++ components/corespring/multiple-choice/src/client/render.js
@@ -164,7 +164,6 @@ var main = [
       replace: true,
       link: link,
       template: [ '<div class="view-multiple-choice">',
-                  '  <directive-test></directive-test>',
                   '  <label ng-bind-html-unsafe="question.prompt"></label>',
                   '  <div class="choices-container" ng-class="question.config.orientation">',
                   '  <div ng-repeat="o in choices" class="choice-holder" ng-class="question.config.orientation">',
