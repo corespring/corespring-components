e85eb44c3165ce041771e2e4832ff82ea051bbd2
Attila Miklosi <attila.miklosi@gmail.com>
2013-11-29 12:25:25 +0100

    fix tests

--- components/corespring/multiple-choice/src/server/index.js
+++ components/corespring/multiple-choice/src/server/index.js
@@ -140,7 +140,6 @@ exports.respond = function(question, answer, settings) {
   response = {
     correctness: answerIsCorrect ? "correct" : "incorrect",
     score: calculateScore(question, answer),
-    outcome: {"overLimit": true}
   };
   if (settings.showFeedback) {
     response.feedback = buildFeedback(question, answer, settings, answerIsCorrect);
