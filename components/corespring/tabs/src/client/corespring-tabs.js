a4d42abbc42dbb48ee36def8922ac460e1e52b6a
edeustace <ed.eustace@gmail.com>
2013-12-05 18:54:47 +0000

    small tidy up

--- components/corespring/tabs/src/client/corespring-tabs.js
+++ components/corespring/tabs/src/client/corespring-tabs.js
@@ -67,8 +67,7 @@ var tabs = [
           newIdx = $scope.selectedIdx + 1;
           return $scope.changeTab(newIdx);
         };
-        
-        this.previousTab = function() {
+         this.previousTab = function() {
           var newIdx;
           newIdx = $scope.selectedIdx - 1;
           return $scope.changeTab(newIdx);
@@ -85,37 +84,6 @@ var tabs = [
                     '<a href="" ng-class="{active: tab.selected()}" ng-click="selectTab(tab, $event)">{{tab.title}}</a>',
                     '</li></ul></div><div class="tab-box" ng-transclude></div></div>'].join('')
 
-      };
-    }
-  ];
-
-var tab = [
-    function() {
-      var linkFn, nextTab;
-      nextTab = 0;
-      linkFn = function(scope, elm, attrs, container) {
-        var tab;
-        tab = {
-          title: attrs['title'],
-          selected: function(newVal) {
-            if (newVal == null) {
-              return scope.selected;
-            }
-            return scope.selected = newVal;
-          }
-        };
-        container.addTab(tab);
-        return scope.$on('$destroy', function() {
-          return container.removeTab(tab);
-        });
-      };
-      return {
-        restrict: 'EA',
-        transclude: true,
-        require: '^corespringTabs',
-        link: linkFn,
-        scope: true,
-        template: "<div class=\"paragraph-style\" ng-class=\"{active:selected}\" ng-show=\"selected\" ng-transclude></div>"
       };
     }
   ];
