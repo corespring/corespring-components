8e6cd833ca1b98d8501f586817f9bf21c1cc31c7
Attila Miklosi <attila.miklosi@gmail.com>
2013-12-26 19:46:21 +0100

    foldable interactions: indication of completeness status of contained interactions wip

--- components/corespring/foldable/src/client/corespring-foldable.js
+++ components/corespring/foldable/src/client/corespring-foldable.js
@@ -1,24 +1,62 @@
 var directive = [
-  '$timeout', '$log', function ($timeout, $log) {
+    '$timeout', '$log', 'ComponentRegister', function ($timeout, $log, ComponentRegister) {
 
-    var linkFn = function(scope, elm, attrs, container) {
-      scope.folded = true;
-    };
+      var linkFn = function (scope, element) {
 
-    return {
-      restrict: 'A',
-      transclude: true,
-      link: linkFn,
-      scope: {},
-      template: [
-        "<div class='view-foldable'>",
-        "<span class='toggle-icon' ng-click='folded = !folded'><img src='/assets/images/component.png'></img></span>",
-        "<div ng-hide='folded' ng-transclude></div>",
-        "</div>"
-      ].join("")
-    };
-  }
-];
+        scope.folded = true;
+
+        var containedIds = [];
+        $(element).find("[id]").each(function (idx, e) {
+          containedIds.push($(e).attr("id"));
+        });
+
+        var isComplete = function () {
+          return !(_.reduce(containedIds, function (memo, elem) {
+            return memo || ComponentRegister.isAnswerEmpty(elem)
+          }, ComponentRegister.isAnswerEmpty(containedIds[0])));
+        };
+
+        var isEmpty = function () {
+          return _.reduce(containedIds, function (memo, elem) {
+            return memo && ComponentRegister.isAnswerEmpty(elem)
+          }, ComponentRegister.isAnswerEmpty(containedIds[0]));
+        };
+
+
+        scope.$watch(
+          function () {
+            return isComplete() + "," + isEmpty();
+          },
+          function (n, o) {
+            scope.isComplete = isComplete();
+            scope.isEmpty = isEmpty();
+          }
+        );
+
+        scope.containerBridge = {
+          setDataAndSession: function (dataAndSession) {
+            $log.debug("foldable setDataAndSession", dataAndSession);
+          }
+        };
+
+      };
+
+      return {
+        restrict: 'A',
+        transclude: true,
+        link: linkFn,
+        scope: {},
+        template: [
+          "<div class='view-foldable'>",
+          "<span class='toggle-icon' ng-click='folded = !folded'><img src='/assets/images/component.png'></img></span>",
+          "<div>Complete: {{isComplete}} Empty: {{isEmpty}}</div>",
+          "<div ng-hide='folded' ng-transclude></div>",
+          "</div>"
+        ].join("")
+      };
+    }
+  ]
+  ;
 
 exports.framework = 'angular';
 exports.directive = {
