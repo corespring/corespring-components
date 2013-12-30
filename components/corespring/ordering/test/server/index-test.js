f2530032cc833bba33301be6c248d99ab9ae59af
Attila Miklosi <attila.miklosi@gmail.com>
2013-12-30 17:47:05 +0100

    basic correctness testing on the server side of the ordering component

--- components/corespring/ordering/test/server/index-test.js
+++ components/corespring/ordering/test/server/index-test.js
@@ -9,52 +9,32 @@ should = require('should');
 _ = require('lodash');
 
 component = {
-  "componentType": "corespring-drag-and-drop",
-  "title": "Butterfly D&D",
-  "correctResponse": {
-    "1": ["egg", "pupa"],
-    "2": [],
-    "3": ["larva","adult"],
-    "4": []
-  },
-  "feedback": [
-    {
-      "feedback": [
-        {
-          "larva": "Great"
-        },
-        {
-          "other": "Not great"
-        }
-      ],
-      "landingPlace": "1"
-    }
-  ],
+  "componentType": "corespring-ordering",
+  "title": "Butterfly Ordering",
+  "correctResponse": ["egg", "pupa","larva","adult"],
   "model": {
-    "answerArea": "<div>A butterfly is first a <span landing-place id='1' class='inline' cardinality='ordered'></landing-place> then a <span landing-place id='2' class='inline'></landing-place> and then a <span landing-place id='3' class='inline'></landing-place> and then a <span landing-place id='4' class='inline'></landing-place> </div>",
     "choices": [
       {
-        "content": "Pupa",
-        "fixed": true,
-        "id": "pupa"
+        "label": "Pupa",
+        "value": "pupa"
       },
       {
-        "content": "Egg",
-        "id": "egg"
+        "label": "Egg",
+        "value": "egg"
       },
       {
-        "content": "Larva",
-        "id": "larva"
+        "label": "Larva",
+        "value": "larva"
       },
       {
-        "content": "Adult",
-        "id": "adult"
+        "label": "Adult",
+        "value": "adult"
       }
     ],
     "config": {
       "shuffle": true
     },
-    "prompt": "Drag the 2nd stage of the butterfly's lifecycle on to the the pod"
+    "prompt": "Drag the stages of the butterfly's lifecycle into order"
   },
   "weight": 1
 };
@@ -71,53 +51,18 @@ settings = function (feedback, userResponse, correctResponse) {
   };
 };
 
-describe('drag and drop server logic', function () {
+describe('ordering server logic', function () {
 
   describe('respond incorrect', function () {
-    var response = server.respond(_.cloneDeep(component), {1: ['larva']}, settings(false, true, true));
+    var response = server.respond(_.cloneDeep(component), ['larva','egg','pupa','adult'], settings(false, true, true));
     response.correctness.should.eql('incorrect');
     response.score.should.eql(0);
   });
 
   describe('respond correct', function () {
-    var answer = {
-      "1": ["egg", "pupa"],
-      "2": [],
-      "3": ["larva", "adult"],
-      "4": []
-    };
-    var response = server.respond(_.cloneDeep(component), answer, settings(false, true, true));
+    var response = server.respond(_.cloneDeep(component), ["egg", "pupa","larva","adult"], settings(false, true, true));
     response.correctness.should.eql('correct');
     response.score.should.eql(1);
-  });
-
-  describe('cardinality is considered', function () {
-    var answer = {
-      "1": ["egg", "pupa"],
-      "2": [],
-      "3": ["larva", "adult"],
-      "4": []
-    };
-    var response = server.respond(_.cloneDeep(component), answer, settings(false, true, true));
-    response.correctness.should.eql('correct');
-
-    answer = {
-      "1": ["pupa", "egg"],     // ordered, so incorrect
-      "2": [],
-      "3": ["larva", "adult"],
-      "4": []
-    };
-    response = server.respond(_.cloneDeep(component), answer, settings(false, true, true));
-    response.correctness.should.eql('incorrect');
-
-    answer = {
-      "1": ["egg", "pupa"],
-      "2": [],
-      "3": ["adult", "larva"],  // not ordered, so correct
-      "4": []
-    };
-    response = server.respond(_.cloneDeep(component), answer, settings(false, true, true));
-    response.correctness.should.eql('correct');
   });
 
 });
