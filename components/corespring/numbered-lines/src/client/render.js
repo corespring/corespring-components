15121f1943838cc4e2624ad4f94b4b6218c544f7
Attila Miklosi <attila.miklosi@gmail.com>
2014-01-23 17:19:48 +0100

    numbered lines interaction

---
+++ components/corespring/numbered-lines/src/client/render.js
@@ -0,0 +1,66 @@
+var main = [
+  '$sce', '$log',
+
+  function ($sce, $log) {
+    var def;
+
+    var link = function (scope, element, attrs) {
+
+
+      scope.containerBridge = {
+
+        setDataAndSession: function (dataAndSession) {
+          scope.lines = dataAndSession.data.model.lines;
+        },
+
+        getSession: function () {
+          return {
+            answers: ""
+          }
+        },
+
+        setResponse: function (response) {
+        },
+
+        setMode : function(newMode) {
+        },
+
+        reset : function(){
+        },
+
+        isAnswerEmpty: function(){
+        },
+
+        answerChangedHandler: function(callback){
+        },
+
+        editable: function(e){
+        }
+      };
+
+      scope.$emit('registerComponent', attrs.id, scope.containerBridge);
+    };
+
+
+    def = {
+      scope: {},
+      restrict: 'AE',
+      replace: true,
+      link: link,
+      template: [
+        '<div class="view-numbered-lines">',
+        '  <ol>',
+        '    <li ng-repeat="line in lines">{{line}}',
+        '    </li>',
+        '  </ol>',
+        '</div>'
+      ].join('')
+    };
+
+    return def;
+  }
+];
+
+exports.framework = 'angular';
+exports.directive = main;
+
