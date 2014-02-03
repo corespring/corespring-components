e4a787cfc499ccf88e8d7b30bba978e002c1aa6a
Attila Miklosi <attila.miklosi@gmail.com>
2014-02-03 14:50:53 +0100

    added tex interaction for latex

---
+++ components/corespring/tex/src/client/corespring-tex.js
@@ -0,0 +1,19 @@
+var directive = function () {
+
+  return {
+    restrict: 'EA',
+    compile: function (element, attrs) {
+      var content = element.html();
+      if (attrs.inline == "false")
+        element.html("$$" + content + "$$");
+      else
+        element.html("\\(" + content + "\\)");
+    }
+  };
+};
+
+exports.framework = 'angular';
+exports.directive = {
+  name: "corespringTex",
+  directive: directive
+};
