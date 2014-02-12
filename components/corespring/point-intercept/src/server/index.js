047564686b5af61b0ff7585b63d2d9a1e5c424a8
Attila Miklosi <attila.miklosi@gmail.com>
2014-02-12 18:01:41 +0100

    fixed handling the feedback logic of point interaction properly

--- components/corespring/point-intercept/src/server/index.js
+++ components/corespring/point-intercept/src/server/index.js
@@ -3,9 +3,16 @@ var _ = require('lodash');
 exports.respond = function (question, answer, settings) {
   var correctResponse = question.correctResponse;
   var isCorrect = _.isEqual(correctResponse, answer);
-  return {
+  var res = {
     "correctness": isCorrect ? "correct" : "incorrect",
     "score": isCorrect ? 1 : 0,
     "correctResponse": correctResponse
   };
+
+  if (settings.showFeedback) {
+    res.outcome = [];
+    if (isCorrect) res.outcome.push('correct'); else res.outcome.push('incorrect');
+  }
+
+  return res;
 };
