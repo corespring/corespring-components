538cb20e8eef23ff6e8638935878f1bd18bac293
rbokel <rbokel.github@qlod.org>
2014-04-03 13:52:36 +0200

    move feeedback config out of feedback component

--- components/corespring/text-entry/src/client/configure.js
+++ components/corespring/text-entry/src/client/configure.js
@@ -1,12 +1,68 @@
 var main = [
   function () {
 
+    var feedbackConfig = {
+      none: {
+        value: "",
+        prompt: "No feedback is given.",
+        tooltip: "Select 'Default' or 'Custom' to show feedback.",
+        readonly: true
+      },
+      positive: {
+        value: "Correct!",
+        prompt: "",
+        tooltip: "Select 'None' to disable or 'Custom' to show customized feedback.",
+        readonly: true
+      },
+      negative: {
+        value: "Try again!",
+        prompt: "",
+        tooltip: "Select 'None' to disable or 'Custom' to show customized feedback.",
+        readonly: true
+      },
+      custom: {
+        value: "",
+        prompt: "Enter custom feedback.",
+        tooltip: "Select 'None' to disable or 'Default' to show the default feedback.",
+        readonly: false
+      }
+    };
+
+    function getFeedbackConfig(type){
+      return {
+        none: feedbackConfig.none,
+        default: feedbackConfig[type],
+        custom: feedbackConfig.custom
+      }
+    }
+
+    function getCorrectResponseFeedbackModel(){
+      return {
+        type: "default",
+        title: "Positive Feedback",
+        headline: "If response is correct",
+        config: getFeedbackConfig('positive')
+      }
+    }
+
+    function getPartialResponseFeedbackModel(){
+      return {
+        type: "default",
+        title: "Negative Feedback",
+        headline: "If response is incorrect or partially correct",
+        config: getFeedbackConfig('negative')
+      };
+    }
+
     return {
       scope: 'isolate',
       restrict: 'E',
       replace: true,
       templateUrl: "/client/libs/corespring/text-entry/templates/design-panel.html",
       link: function (scope, element, attrs) {
+
+        scope.itemId = attrs.id;
+
         scope.containerBridge = {
           setModel: function (fullModel) {
             scope.fullModel = fullModel;
@@ -28,18 +84,12 @@ var main = [
             fullModel.model.config = fullModel.model.config || {};
 
             fullModel.model.config.correctResponse = fullModel.model.config.correctResponse || {};
-            fullModel.model.config.correctResponse.feedback = fullModel.model.config.correctResponse.feedback || {
-              type: "default",
-              title: "Positive Feedback",
-              headline: "If response is correct"
-            };
+            fullModel.model.config.correctResponse.feedback = fullModel.model.config.correctResponse.feedback ||
+              getCorrectResponseFeedbackModel();
 
             fullModel.model.config.partialResponse = fullModel.model.config.partialResponse || {};
-            fullModel.model.config.partialResponse.feedback = fullModel.model.config.partialResponse.feedback || {
-              type: "default",
-              title: "Negative Feedback",
-              headline: "If response is incorrect or partially correct"
-            };
+            fullModel.model.config.partialResponse.feedback = fullModel.model.config.partialResponse.feedback ||
+              getPartialResponseFeedbackModel();
           },
 
           getModel: function () {
@@ -67,41 +117,6 @@ var main = [
 var csFeedbackInput = [
   function () {
 
-    var feedbackConfig = {
-      none: {
-        value: "",
-        prompt: "No feedback is given.",
-        tooltip: "Select 'Default' or 'Custom' to show feedback.",
-        readonly: true
-      },
-      default: {
-        value: "Try again!",
-        prompt: "",
-        tooltip: "Select 'None' to disable or 'Custom' to show customized feedback.",
-        readonly: true
-      },
-      custom: {
-        value: "",
-        prompt: "Enter custom feedback.",
-        tooltip: "Select 'None' to disable or 'DefauLt' to show the default feedback.",
-        readonly: false
-      }
-    };
-
-    function setFeedbackType(feedback, newType) {
-      function assignFeedbackConfig(config) {
-        _.extend(feedback, config);
-      }
-
-      if (newType === 'none') {
-        assignFeedbackConfig(feedbackConfig.none);
-      } else if (newType === 'default') {
-        assignFeedbackConfig(feedbackConfig.default);
-      } else if (newType === 'custom') {
-        assignFeedbackConfig(feedbackConfig.custom);
-      }
-    }
-
     return {
       scope: {
         feedback: '=model'
@@ -112,7 +127,7 @@ var csFeedbackInput = [
       link: function (scope, element, attrs) {
 
         scope.$watch('feedback.type', function (newType) {
-          setFeedbackType(scope.feedback, newType);
+          _.extend(scope.feedback, scope.feedback.config[newType]);
         });
       }
     };
