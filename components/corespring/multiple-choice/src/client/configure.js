14729d5c9f12587531791a21883be1418a84b73c
Attila Miklosi <attila.miklosi@gmail.com>
2014-04-08 10:07:16 +0200

    Preview of default feedback in the config panel of MC ( https://thesib.atlassian.net/browse/CA-986 )

--- components/corespring/multiple-choice/src/client/configure.js
+++ components/corespring/multiple-choice/src/client/configure.js
@@ -38,6 +38,11 @@ var main = [
       replace: true,
       link: function(scope, element, attrs) {
 
+        // TODO: this needs to be centralised and not duplicated here and the server side
+        scope.defaultCorrectFeedback = "Correct!";
+        scope.defaultIncorrectFeedback = "Good try but that is not the correct answer";
+        scope.defaultNotChosenFeedback = "This answer is correct";
+
         scope.containerBridge = {
           setModel: function(model) {
             scope.fullModel = model;
