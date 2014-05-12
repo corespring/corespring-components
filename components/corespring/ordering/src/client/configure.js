611cb8f8ae034775502e8c49ccdf42fb67cee7e8
benburton <benjamin.joseph.burton@gmail.com>
2014-05-12 08:53:49 -0400

    Beginning to introduce mini-wiggi

--- components/corespring/ordering/src/client/configure.js
+++ components/corespring/ordering/src/client/configure.js
@@ -10,6 +10,7 @@ var main = [
           setModel: function(model) {
             $scope.fullModel = model;
             $scope.model = $scope.fullModel.model;
+            $scope.deactivate();
           },
           getModel: function() {
             var model = _.cloneDeep($scope.fullModel);
@@ -21,6 +22,22 @@ var main = [
           $scope.model.choices.splice(index, 1);
         };
 
+        $scope.activate = function($index) {
+          $scope.active[$index] = true;
+
+          $element.find('.text-field').each(function(i, el) {
+            $scope.active[i] = $index === i;
+            if ($index === i) {
+              $(el).focus();
+            }
+          });
+        };
+
+        $scope.deactivate = function() {
+          console.log($scope.model.choices);
+          $scope.active = _.map($scope.model.choices, function() { return false; });
+        };
+
         $scope.addChoice = function() {
           $scope.model.choices.push({content: "", label: ""});
         }
@@ -28,7 +45,7 @@ var main = [
         $scope.$emit('registerConfigPanel', $attrs.id, $scope.containerBridge);
       },
       template: [
-        '<div class="view-ordering-config">',
+        '<div class="view-ordering-config" ng-click="deactivate()">',
         '  <p class="info">In Ordering, a student is asked to sequence events or inputs in a specific order.</p>',
         '  <p class="info">',
         '    Drag and drop your choices to set the correct order. The student view will display the choices in ',
@@ -36,12 +53,12 @@ var main = [
         '  </p>',
         '  <input class="prompt" type="text" ng-model="model.prompt" placeholder="Enter a label or leave blank"/>',
         '  <ul ui-sortable="" ng-model="model.choices">',
-        '    <li ng-repeat="choice in model.choices" ng-mouseenter="active = true" ng-mouseleave="active = false">',
-        '      <div class="delete-icon" ng-show="active">',
+        '    <li ng-repeat="choice in model.choices" ng-click="$event.stopPropagation()" ng-dblclick="activate($index)">',
+        '      <div class="delete-icon" ng-show="active[$index]">',
         '        <i ng-click="deleteChoice($index)" class="fa fa-times-circle"></i>',
         '      </div>',
-        '      <span ng-hide="active">{{choice.label}}</span>',
-        '      <input type="text" ng-show="active" ng-model="choice.label" />',
+        '      <span ng-hide="active[$index]">{{choice.label}}</span>',
+        '      <input type="text" ng-show="active[$index]" ng-model="choice.label" mini-wiggi-wiz />',
         '    </li>',
         '  </ul>',
         '  <button class=\"btn\" ng-click=\"addChoice()\">Add a Choice</button>',
