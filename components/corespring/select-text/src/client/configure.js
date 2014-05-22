ff2caa95c01442884e86bf3bfb29c0e158f12cb0
benburton <benjamin.joseph.burton@gmail.com>
2014-05-22 15:59:48 -0400

    Basic config panel for select text

--- components/corespring/select-text/src/client/configure.js
+++ components/corespring/select-text/src/client/configure.js
@@ -6,11 +6,56 @@ var main = [
       restrict: 'E',
       replace: true,
       link: function($scope, $element, $attrs) {
+        $scope.containerBridge = {
+          setModel: function(model) {
+            console.log(model);
+            $scope.fullModel = model;
+            $scope.model = $scope.fullModel.model;
+          },
+          getModel: function() {
+            var model = _.cloneDeep($scope.fullModel);
+            return model;
+          }
+        };
 
+        $scope.$emit('registerConfigPanel', $attrs.id, $scope.containerBridge);
       },
       template: [
+        '<div class="select-text-configuration">',
+        '  <div navigator="">',
+        '    <div navigator-panel="Design">',
+        '      <div class="input-holder">',
+        '        <div class="body">',
+        '          <p class="info">',
+        '            In select text...',
+        '          </p>',
+        '          <input class="prompt" type="text" ng-model="model.prompt"/>',
+        '          <wiggi-wiz ng-model="model.text"></wiggi-wiz>',
+        '          <div class="property">',
+        '            <label>Selection Unit</label>',
+        '            <select class="form-control" ng-model="model.config.selectionUnit">',
+        '              <option value="word">Word</option>',
+        '              <option value="sentence">Sentence</option>',
+        '            </select>',
+        '          </div>',
+        '          <div class="property">',
+        '            <label>Minimum Selections</label>',
+        '            <input type="number" min="1" max="99" class="form-control" ',
+        '              ng-model="model.config.minSelections"/>',
+        '          </div>',
+        '          <div class="property">',
+        '            <label>Maximum Selections</label>',
+        '            <input type="number" min="1" max="99" class="form-control" ',
+        '              ng-model="model.config.maxSelections"/>',
+        '          </div>',
+        '        </div>',
+        '      </div>',
+        '    </div>',
+        '  </div>',
+        '</div>'
       ].join("")
     };
+
   }
 ];
 
