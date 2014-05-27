2e9d868a97f46c4300dd11ddd2bf0340c7cda5dc
benburton <benjamin.joseph.burton@gmail.com>
2014-05-27 09:19:05 -0400

    Setting up express to serve js assets

--- components/corespring/multiple-choice/test/regression/test.js
+++ components/corespring/multiple-choice/test/regression/test.js
@@ -1,15 +1,25 @@
 'use strict';
 
+var express = require('express');
 var should = require('should');
 
 describe('multiple-choice', function() {
 
+  beforeEach(function() {
+    var app = express();
+
+    app.use(express.static(__dirname + '/../../src/client'));
+
+    app.listen(3000);
+
+  })
+
   it('displays correct feedback when answered', function(done) {
 
     browser
-      .url('http://corespring.org')
-      .title(function(err, title) {
-        title.value.should.eql('CoreSpring');
+      .url('http://localhost:3000/render.js')
+      .getText('body', function(error, body) {
+        console.log(body);
       })
       .call(done);
 
