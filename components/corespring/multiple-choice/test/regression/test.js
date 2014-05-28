9d297dfdf72bf753a5c723f86daf5e68b4e965d5
benburton <benjamin.joseph.burton@gmail.com>
2014-05-28 14:31:26 -0400

    Add regression test to check for correct/incorrect

--- components/corespring/multiple-choice/test/regression/test.js
+++ components/corespring/multiple-choice/test/regression/test.js
@@ -1,22 +1,67 @@
 'use strict';
 
-var express = require('express');
 var should = require('should');
+var fs = require('fs');
+
+var Regression = {
+  getUrl: function(componentType, jsonFile) {
+    return 'http://localhost:9000/client/rig/corespring-' + componentType + '/index.html?data=regression_' + jsonFile;
+  },
+  getItemJson: function(jsonFile) {
+    return require('./../../regression-data/' + jsonFile);
+  }
+};
 
 describe('multiple-choice', function() {
+  var filename = 'one.json';
+  var itemJson = Regression.getItemJson(filename);
+  var correctAnswer = itemJson.item.components['1'].correctResponse.value[0];
 
-  beforeEach(function() {
-    var app = express();
-    app.use(express.static(__dirname + '/../../src/client'));
-    app.listen(3000);
-  });
+  console.log(Regression.getUrl('multiple-choice', filename));
 
-  it('displays correct feedback when answered', function(done) {
+  it('does not display incorrect feedback when correct answer selected', function(done) {
 
     browser
-      .url('http://localhost:3000/render.js')
-      .getText('body', function(error, body) {
-        console.log(body);
+      .url(Regression.getUrl('multiple-choice', filename))
+      .waitFor('.choice-input input', 2000)
+      .elements('.choice-input input', function(err, results) {
+        for (var i = 0; i < results.value.length; i++) {
+          (function(i) {
+            browser.elementIdAttribute(results.value[i].ELEMENT, 'value', function(err, res) {
+              if (res.value === correctAnswer) {
+                browser.elementIdClick(results.value[i].ELEMENT);
+              }
+            })
+          })(i);
+        }
+      })
+      .execute('window.submit()')
+      .isVisible('.choice-holder.incorrect', function(err, result) {
+        (result === null).should.be.ok;
+      })
+      .call(done);
+
+  });
+
+  it('displays incorrect feedback when incorrect answer selected', function(done) {
+
+    browser
+      .url(Regression.getUrl('multiple-choice', filename))
+      .waitFor('.choice-input input', 2000)
+      .elements('.choice-input input', function(err, results) {
+        for (var i = 0; i < results.value.length; i++) {
+          (function(i) {
+            browser.elementIdAttribute(results.value[i].ELEMENT, 'value', function(err, res) {
+              if (res.value !== correctAnswer) {
+                browser.elementIdClick(results.value[i].ELEMENT);
+              }
+            })
+          })(i);
+        }
+      })
+      .execute('window.submit()')
+      .isVisible('.choice-holder.incorrect', function(err, result) {
+        (result === null).should.not.be.ok;
       })
       .call(done);
 
