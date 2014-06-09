1c979135320764fd8e0664b877e330c571958443
edeustace <ed.eustace@gmail.com>
2014-06-09 13:12:00 +0100

    test files

---
+++ components/corespring/choice-templates/src/server/index.js
@@ -0,0 +1,6 @@
+console.log('---------------> choice templates!!');
+var imageService = require('corespring.image-service.server');
+
+exports.blah = function(){
+  return imageService.blah();
+}
---
+++ components/corespring/image-service/src/server/index.js
@@ -0,0 +1,5 @@
+
+console.log('---------------> image service!!');
+exports.blah = function(){
+  return 'blah...';
+}
