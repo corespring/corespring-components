d7d00074582f4bdd3248f3b70675c4705016e0bf
rbokel <rbokel.github@qlod.org>
2014-06-19 14:24:40 +0200

    simplified the template

--- components/corespring/choice-templates/src/client/index.js
+++ components/corespring/choice-templates/src/client/index.js
@@ -82,86 +82,83 @@ exports.service = ['$log',
           }
         };
 
-        return _.flatten([ //TODO Is this really necessary? Why is it a 2 dim array?
-          [
-            '  <table class="choice-template-choice">',
-            '    <tr>', !_.isEmpty(opts.choice) && opts.showLabel ? ' <td ' + optWidth(opts.columnWidths[0]) + '>' + opts.choice + '</td>' : ''
-          ],
-          [
-            '     <td ' + optWidth(opts.columnWidths[1]) + '>',
-            '       <div class="choice-wrapper">',
-            '         <span class="choice-remove-button" ng-click="removeQuestion(q)">',
-            '           <i class="fa fa-times-circle"></i>',
-            '         </span>',
-            '       </div>',
-            '       <div mini-wiggi-wiz="" ng-model="q.label" placeholder="Enter a choice"',
-            '         image-service="imageService()" features="extraFeatures" feature-overrides="overrideFeatures"',
-            '         parent-selector=".wiggi-wiz-overlay"></div>',
-            '       <label class="shuffle" ng-class="{shown: model.config.shuffle}">',
-            '         <input type="checkbox" ',
-            '           ng-init="remain = q.shuffle == undefined ? false : !q.shuffle" ng-model="remain"',
-            '           ng-change="q.shuffle = !remain; resetStash()" /> Remain in place',
-            '       </label>',
-            '     </td>',
-            '     <td class="correct" ' + optWidth(opts.columnWidths[2]) + '>',
-            opts.correct,
-            '      </td>',
-            '    </tr>',
-            '    <tr>',
-            feedback,
-            '    </tr>',
-            '  </table>'
-          ]
-        ]).join('\n');
-
-      };
-
-      this.wrap = function(title, body) {
-        return ['<div class="input-holder">',
-          title ? '  <div class="header">' + title + '</div>' : '',
-          '  <div class="body">' + body + '</div>',
-          '</div>'
-        ].join('\n');
-      };
-
-
-      this.scoring = function(opts) {
-
-        var o = _.extend({
-          maxNumberOfPartialScores: "model.choices.length - 1"
-        }, opts);
-
         return [
-          '<div class="scoring-header-text">',
-          '  If there is more than one correct answer to this question, you may allow partial credit based on the number of correct answers submitted. This is optional.',
-          '</div>',
-
-          '<div>',
-          '   <input id="partialScoring" type="checkbox" ng-model="fullModel.allowPartialScoring" ng-disabled="isSingleChoice()"></input>',
-          '   <label for="partialScoring" ng-class="{ disabled: isSingleChoice() }">Allow partial scoring</label>',
-          '</div>',
-
-          '<form class="form-inline choice-template-scoring-section">',
-          '  <div class="well scoring-well" ng-show="fullModel.allowPartialScoring">',
-          '    <div class="score-row" ng-repeat="scenario in fullModel.partialScoring">',
-          '      <div class="remove-button" ng-click="removeScoringScenario(scenario)">',
-          '        <button type="button" class="close">&times;</button>',
-          '      </div>',
-          '      <label>If</label> <div class="form-group"><input type="number" min="1" max="{{model.choices.length - 1}}" style="width: 60px" class="form-control {{validClass(scenario)}}" ng-model="scenario.numberOfCorrect"/></div> of correct answers selected, award',
-          '      <span class="form-group"><input type="number" min="1" max="99" style="width: 60px" class="form-control" ng-model="scenario.scorePercentage"/>% of full credit</span>',
-          '    </div>',
-          '    <div ng-click="addScoringScenario()" ng-show="fullModel.partialScoring.length < ' + o.maxNumberOfPartialScores + '">',
-          '      <i class="fa fa-plus-square-o"></i> Add another scenario',
-          '    </div>',
-          '  </div>',
-          '</form>',
-
-          '<div>{{model.partialScoring}}</div>'
-
+          '<table class="choice-template-choice">',
+          '  <tr>',
+          !_.isEmpty(opts.choice) && opts.showLabel ? ' <td ' + optWidth(opts.columnWidths[0]) + '>' + opts.choice + '</td>' : '',
+          '   <td ' + optWidth(opts.columnWidths[1]) + '>',
+          '     <div class="choice-wrapper">',
+          '       <span class="choice-remove-button" ng-click="removeQuestion(q)">',
+          '         <i class="fa fa-times-circle"></i>',
+          '       </span>',
+          '     </div>',
+          '     <div mini-wiggi-wiz="" ng-model="q.label" placeholder="Enter a choice"',
+          '       image-service="imageService()" features="extraFeatures" feature-overrides="overrideFeatures"',
+          '       parent-selector=".wiggi-wiz-overlay"></div>',
+          '     <label class="shuffle" ng-class="{shown: model.config.shuffle}">',
+          '       <input type="checkbox" ',
+          '         ng-init="remain = q.shuffle == undefined ? false : !q.shuffle" ng-model="remain"',
+          '         ng-change="q.shuffle = !remain; resetStash()" /> Remain in place',
+          '     </label>',
+          '   </td>',
+          '   <td class="correct" ' + optWidth(opts.columnWidths[2]) + '>',
+          opts.correct,
+          '    </td>',
+          '  </tr>',
+          '  <tr>',
+          feedback,
+          '  </tr>',
+          '</table>'
         ].join('\n');
-      };
-    }
 
-    return new ChoiceTemplates();
+    };
+
+    this.wrap = function(title, body) {
+      return ['<div class="input-holder">',
+        title ? '  <div class="header">' + title + '</div>' : '',
+        '  <div class="body">' + body + '</div>',
+        '</div>'
+      ].join('\n');
+    };
+
+
+    this.scoring = function(opts) {
+
+      var o = _.extend({
+        maxNumberOfPartialScores: "model.choices.length - 1"
+      }, opts);
+
+      return [
+        '<div class="scoring-header-text">',
+        '  If there is more than one correct answer to this question, you may allow partial credit based on the number of correct answers submitted. This is optional.',
+        '</div>',
+
+        '<div>',
+        '   <input id="partialScoring" type="checkbox" ng-model="fullModel.allowPartialScoring" ng-disabled="isSingleChoice()"></input>',
+        '   <label for="partialScoring" ng-class="{ disabled: isSingleChoice() }">Allow partial scoring</label>',
+        '</div>',
+
+        '<form class="form-inline choice-template-scoring-section">',
+        '  <div class="well scoring-well" ng-show="fullModel.allowPartialScoring">',
+        '    <div class="score-row" ng-repeat="scenario in fullModel.partialScoring">',
+        '      <div class="remove-button" ng-click="removeScoringScenario(scenario)">',
+        '        <button type="button" class="close">&times;</button>',
+        '      </div>',
+        '      <label>If</label> <div class="form-group"><input type="number" min="1" max="{{model.choices.length - 1}}" style="width: 60px" class="form-control {{validClass(scenario)}}" ng-model="scenario.numberOfCorrect"/></div> of correct answers selected, award',
+        '      <span class="form-group"><input type="number" min="1" max="99" style="width: 60px" class="form-control" ng-model="scenario.scorePercentage"/>% of full credit</span>',
+        '    </div>',
+        '    <div ng-click="addScoringScenario()" ng-show="fullModel.partialScoring.length < ' + o.maxNumberOfPartialScores + '">',
+        '      <i class="fa fa-plus-square-o"></i> Add another scenario',
+        '    </div>',
+        '  </div>',
+        '</form>',
+
+        '<div>{{model.partialScoring}}</div>'
+
+      ].join('\n');
+    };
   }
+
+  return new ChoiceTemplates();
+}
 ];
