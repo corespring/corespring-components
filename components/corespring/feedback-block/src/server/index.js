9a89692259c5030287bc72633bc7d38487e92f96
rbokel <rbokel.github@qlod.org>
2014-07-15 16:15:06 +0200

    fix semicolon

--- components/corespring/feedback-block/src/server/index.js
+++ components/corespring/feedback-block/src/server/index.js
@@ -11,7 +11,7 @@ exports.isCorrect = function() {
 exports.respond = function(model, answer, settings, targetOutcome) {
 
   function findFeedback(feedbacks, response){
-    var o =  _.find(feedbacks, function(item){return item && item.input === response});
+    var o =  _.find(feedbacks, function(item){return item && item.input === response;});
     return o ? o.feedback : "";
   }
 
