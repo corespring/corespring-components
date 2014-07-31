695d647984bd971cca80d6857e1495fec8979056
edeustace <ed.eustace@gmail.com>
2014-07-31 20:39:45 +0100

    fix formatting

--- components/corespring/feedback-block/src/server/index.js
+++ components/corespring/feedback-block/src/server/index.js
@@ -1,4 +1,4 @@
-  var _ = require('lodash');
+var _ = require('lodash');
 
 exports.render = function(element) {
   return element;
@@ -11,9 +11,9 @@ exports.isCorrect = function() {
 exports.respond = function(model, answer, settings, targetOutcome) {
 
 
-  if(!targetOutcome || _.isEmpty(targetOutcome)){
+  if (!targetOutcome || _.isEmpty(targetOutcome)) {
     console.warn('target outcome is empty!', JSON.stringify(model));
-    
+
     return {
       correctness: 'incorrect',
       feedback: {}
@@ -21,7 +21,7 @@ exports.respond = function(model, answer, settings, targetOutcome) {
   }
 
   function findFeedback(feedbacks, response) {
-    var o =  _.find(feedbacks, function(item) {
+    var o = _.find(feedbacks, function(item) {
       return item && (response.toLowerCase().replace(/ /g, "") === item.input.toLowerCase().replace(/ /g, ""));
     });
     return o ? o.feedback : "";
@@ -41,13 +41,13 @@ exports.respond = function(model, answer, settings, targetOutcome) {
   if (feedback) {
     isCorrect = true;
   } else {
-    feedback = findFeedback(incorrectFeedback,targetOutcome.studentResponse);
+    feedback = findFeedback(incorrectFeedback, targetOutcome.studentResponse);
     isCorrect = false;
   }
 
   if (!feedback) {
     isCorrect = targetOutcome.correctness === "correct";
-    feedback = findFeedback(isCorrect ? correctFeedback : incorrectFeedback,"*");
+    feedback = findFeedback(isCorrect ? correctFeedback : incorrectFeedback, "*");
   }
 
   if (targetOutcome.outcome) {
