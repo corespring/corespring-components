08dc841eff354aff197e9fc3735ff77b402817f0
benburton <benjamin.joseph.burton@gmail.com>
2014-09-08 14:53:41 -0400

    Trying something else

--- components/corespring/checkbox/src/client/checkbox.js
+++ components/corespring/checkbox/src/client/checkbox.js
@@ -2,29 +2,40 @@ exports.framework = "angular";
 exports.directive = {
   name: "checkbox",
   directive: [
-    function() {
+    '$compile',
+    function($compile) {
 
-      function link($scope) {
+      // Attributes to be passed from containing <checkbox/> to <input type='checkbox'/> in rendered markup
+      var attributes = ['ngModel', 'ngDisabled', 'ngChecked'];
+
+      function dasher(str) {
+        return str.replace(/([a-z][A-Z])/g, function (g) { return g[0] + '-' + g[1].toLowerCase() });
       }
 
       return {
         restrict: 'E',
-        replace: true,
-        transclude: true,
-        link: link,
-        scope: {
-          model: '=',
-          value: '='
-        },
-        template: [
-          '<div class="checkbox">',
-          '  <label>',
-          '    <div class="checkbox-toggle" ng-class="{\'checked\': model}" ng-click="switch($event)"/>',
-          '    <input type="checkbox" ng-model="model" /><span class="label-text" ng-transclude/>',
-          '  </label>',
-          '</div>'
-        ].join("\n")
+        link : function (scope, element, attrs) {
+          var attrStr = [];
+          for (var i in attributes) {
+            var key = attributes[i];
+            var value = attrs[attributes[i]];
+            if (value) {
+              attrStr.push(dasher(key) + '=' + "'" + value + "'");
+            }
+          }
+          var tmpl = [
+            '<div class="checkbox">',
+            '  <label>',
+            '    <div class="checkbox-toggle" ng-class="{\'checked\': checked}"/>',
+            '    <input type="checkbox" ' + attrStr.join(' ') + '/>',
+            '  </label>',
+            '</div>'
+          ].join('');
 
+          var compiled = $compile(tmpl)(scope);
+          element.empty();
+          element.append(compiled);
+        }
       };
     }
   ]
