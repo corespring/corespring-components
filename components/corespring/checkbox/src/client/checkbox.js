437b2360b1ecfddef8d140667d86aa3d97700006
benburton <benjamin.joseph.burton@gmail.com>
2014-09-08 16:54:33 -0400

    Transclude labels

--- components/corespring/checkbox/src/client/checkbox.js
+++ components/corespring/checkbox/src/client/checkbox.js
@@ -5,23 +5,28 @@ exports.directive = {
     '$compile',
     function($compile) {
 
+      var link = function($scope, $element, $attr) {
+        $scope.toggle = function() {
+          $scope.model = ($scope.model === true ? false : true);
+        };
+        $attr.$observe('checked', function(checked) {
+          $scope.model = (checked === true);
+        });
+      };
+
       return {
         replace: true,
         restrict: 'E',
+        transclude: true,
         scope: {
-          model: '@'
+          model: '=?'
         },
-        link: function($scope, $element, $attr) {
-          $scope.toggle = function() {
-            $scope.model = ($scope.model === true ? false : true);
-          };
-          $attr.$observe('checked', function(checked) {
-            $scope.model = (checked === true);
-          });
+        compile: function($scope, $element, $trans) {
+          return link;
         },
         template: [
-          '<div class="checkbox-input">',
-          '  <div class="checkbox-toggle" ng-click="toggle()" ng-class="{\'checked\': model}"/>',
+          '<div class="checkbox-input" ng-click="toggle()">',
+          '  <div class="checkbox-toggle" ng-class="{\'checked\': model}"/><span class="label-text" ng-transclude/>',
           '</div>'
         ].join('')
       };
