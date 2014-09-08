d04add04d4d90f10bfa5ccbd35838e3e6dedcea4
benburton <benjamin.joseph.burton@gmail.com>
2014-09-08 15:28:14 -0400

    Don't use <input/>

--- components/corespring/checkbox/src/client/checkbox.js
+++ components/corespring/checkbox/src/client/checkbox.js
@@ -5,37 +5,23 @@ exports.directive = {
     '$compile',
     function($compile) {
 
-      // Attributes to be passed from containing <checkbox/> to <input type='checkbox'/> in rendered markup
-      var attributes = ['ngModel', 'ngDisabled', 'ngChecked'];
-
-      function dasher(str) {
-        return str.replace(/([a-z][A-Z])/g, function (g) { return g[0] + '-' + g[1].toLowerCase() });
-      }
-
       return {
+        replace: true,
         restrict: 'E',
-        link : function (scope, element, attrs) {
-          var attrStr = [];
-          for (var i in attributes) {
-            var key = attributes[i];
-            var value = attrs[attributes[i]];
-            if (value) {
-              attrStr.push(dasher(key) + '=' + "'" + value + "'");
-            }
-          }
-          var tmpl = [
-            '<div class="checkbox">',
-            '  <label>',
-            '    <div class="checkbox-toggle" ng-class="{\'checked\': checked}"/>',
-            '    <input type="checkbox" ' + attrStr.join(' ') + '/>',
-            '  </label>',
-            '</div>'
-          ].join('');
-
-          var compiled = $compile(tmpl)(scope);
-          element.empty();
-          element.append(compiled);
-        }
+        scope: {
+          model: '@'
+        },
+        link: function($scope, $element) {
+          $scope.toggle = function() {
+            $scope.model = ($scope.model === true ? false : true);
+          };
+          console.log($element.find('.checkbox-toggle'));
+        },
+        template: [
+          '<div class="checkbox-input">',
+          '  <div class="checkbox-toggle" ng-click="toggle()" ng-class="{\'checked\': model}"/>',
+          '</div>'
+        ].join('')
       };
     }
   ]
