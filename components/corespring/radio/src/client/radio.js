193c5fd3bfe8551da2696c8fe4a16917e64bebeb
benburton <benjamin.joseph.burton@gmail.com>
2014-09-11 09:47:37 -0400

    WIP

--- components/corespring/radio/src/client/radio.js
+++ components/corespring/radio/src/client/radio.js
@@ -3,16 +3,37 @@ exports.directive = {
   name: 'radio',
   directive: [
     function() {
+
+      var ngModelLink = function(scope, element, attr, ctrl) {
+        element.on('click', function() {
+          if (element[0].checked) {
+            scope.$apply(function() {
+              ctrl.$setViewValue(attr.value);
+            });
+          }
+        });
+
+        scope.$watch(attrs.ngModel, function(value) {
+          
+        });
+
+      };
+
       return {
         replace: true,
         restrict: 'E',
         transclude: true,
         require: ['?ngModel'],
         link: function(scope, element, attr, ctrls) {
-          if (attr.name) {
+          if (angular.isUndefined(attr.name)) {
+            //element.attr('name', nextUid()); // need to see how angular gets this in here.
+          } else {
             element.attr('name', attr.name);
           }
-          // do the link stuff here.
+
+          if (ctrls[0]) {
+            ngModelLink(scope, element, attr, ctrls[0]);
+          }
         },
         template: [
           '<div class="radio-input">',
