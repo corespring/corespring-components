1a43be1b3829c64f475fd4ac0ae9c9bcd0821d18
benburton <benjamin.joseph.burton@gmail.com>
2014-10-06 09:02:48 -0400

    Remove unneeded function

--- components/corespring/multiple-choice/test/client/configure-test.js
+++ components/corespring/multiple-choice/test/client/configure-test.js
@@ -82,20 +82,6 @@ describe('corespring', function() {
       });
     });
 
-    function printNode(who){
-      var deep = true;
-      if(!who || !who.tagName) return '';
-      var txt, ax, el= document.createElement("div");
-      el.appendChild(who.cloneNode(false));
-      txt= el.innerHTML;
-      if(deep){
-        ax= txt.indexOf('>')+1;
-        txt= txt.substring(0, ax)+who.innerHTML+ txt.substring(ax);
-      }
-      el= null;
-      return txt;
-    }
-
     beforeEach(inject(function($compile, $rootScope) {
       scope = $rootScope.$new();
       container = new MockComponentRegister();
