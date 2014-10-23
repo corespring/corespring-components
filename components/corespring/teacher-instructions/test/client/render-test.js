ec774d21c9e001461e0440864e44f75b538c8462
benburton <benjamin.joseph.burton@gmail.com>
2014-10-23 14:56:06 -0400

    Remove unneeded function

--- components/corespring/teacher-instructions/test/client/render-test.js
+++ components/corespring/teacher-instructions/test/client/render-test.js
@@ -2,19 +2,6 @@ describe("corespring", function() {
 
   var rootScope, element;
 
-  function getHtml(who, deep){
-    if(!who || !who.tagName) return '';
-    var txt, ax, el= document.createElement("div");
-    el.appendChild(who.cloneNode(false));
-    txt= el.innerHTML;
-    if(deep){
-      ax= txt.indexOf('>')+1;
-      txt= txt.substring(0, ax)+who.innerHTML+ txt.substring(ax);
-    }
-    el= null;
-    return txt;
-  }
-
   beforeEach(angular.mock.module('test-app'));
 
   beforeEach(inject(function($compile, $rootScope) {
