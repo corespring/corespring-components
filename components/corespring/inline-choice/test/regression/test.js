b9ddc8837885f00828da54b4ea07b8f05527fb89
Attila Miklosi <attila.miklosi@gmail.com>
2015-01-22 14:15:18 +0100

    added regressions tests

--- components/corespring/inline-choice/test/regression/test.js
+++ components/corespring/inline-choice/test/regression/test.js
@@ -9,22 +9,25 @@ var RegressionHelper = (function() {
   return new RegressionHelperDef(regressionTestRunnerGlobals.baseUrl);
 })();
 
-describe.only('inline-choice', function() {
+var inlineChoiceWithId = function(id) {
+  return '//div[@id="' + id + '" and @class[contains(., "view-inline-choice")]]';
+};
+
+describe('inline-choice', function() {
 
   "use strict";
 
   var itemJsonFilename = 'one.json';
   var itemJson = RegressionHelper.getItemJson('inline-choice', itemJsonFilename);
 
-  browser.selectInlineChoice = function(choice) {
-    console.log("Selecting");
-    browser.click('.dropdown-toggle');
-    browser.click('//div[text()="' + choice + '"]');
+
+  browser.selectInlineChoice = function(id, choice) {
+    browser.click(inlineChoiceWithId(id) + '//span[@class[contains(., "dropdown-toggle")]]');
+    browser.click(inlineChoiceWithId(id) + '//div[text()="' + choice + '"]');
     return this;
   };
 
   browser.submitItem = function() {
-    console.log("submitting");
     this.execute('window.submit()');
     return this;
   };
@@ -35,11 +38,31 @@ describe.only('inline-choice', function() {
       .waitFor('.dropdown-menu li', regressionTestRunnerGlobals.defaultTimeout);
   });
 
-
-  it('displays feedback correctly', function(done) {
+  it('feedbacks are positioned correctly', function(done) {
     browser
-      .selectInlineChoice("Banana")
-      .waitFor('.cica')
+      .selectInlineChoice("1", "Banana")
+      .selectInlineChoice("2", "Apple")
+      .submitItem()
+      .click(inlineChoiceWithId("1") + '//div')
+      .click(inlineChoiceWithId("2") + '//div')
+      .getLocation(".player-body", function(err, playerPos) {
+        this.getLocation(inlineChoiceWithId("1") + "//div[@class='arrow']", function(err, arrowPos) {
+          this.getLocation(inlineChoiceWithId("1") + "//div[@class='popover-content']", function(err, popupPos) {
+            arrowPos.x.should.be.above(popupPos.x);
+            arrowPos.x.should.be.below(popupPos.x + 200); // popover has a fixed width of 200px
+
+            popupPos.x.should.be.above(playerPos.x);
+          });
+        });
+        this.getLocation(inlineChoiceWithId("2") + "//div[@class='arrow']", function(err, arrowPos) {
+          this.getLocation(inlineChoiceWithId("2") + "//div[@class='popover-content']", function(err, popupPos) {
+            arrowPos.x.should.be.above(popupPos.x);
+            arrowPos.x.should.be.below(popupPos.x + 200); // popover has a fixed width of 200px
+
+            popupPos.x.should.be.above(playerPos.x);
+          });
+        });
+      })
       .call(done);
 
   });
