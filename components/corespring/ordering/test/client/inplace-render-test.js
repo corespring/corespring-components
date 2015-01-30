d5d6ad97e2338fac29651909fd72fd0ceac9987f
Attila Miklosi <attila.miklosi@gmail.com>
2015-01-30 13:11:28 +0100

    some more tests

--- components/corespring/ordering/test/client/inplace-render-test.js
+++ components/corespring/ordering/test/client/inplace-render-test.js
@@ -61,9 +61,18 @@ describe('corespring', function() {
       rootScope = $rootScope;
     }));
 
-    it('defaults to inplace ordering', function() {
-      container.elements['1'].setDataAndSession(verticalModel);
+    function setModelAndDigest(model) {
+      container.elements['1'].setDataAndSession(model);
       scope.$digest();
+    }
+
+    function setResponseAndDigest(response) {
+      container.elements['1'].setResponse(response);
+      scope.$digest();
+    }
+
+    it('defaults to inplace ordering', function() {
+      setModelAndDigest(verticalModel);
       expect($(element).find('.view-ordering').length).toBeGreaterThan(0);
     });
 
@@ -72,27 +81,36 @@ describe('corespring', function() {
         expect(element.html()).toBeDefined();
       });
 
+      it('feedback is hidden', function() {
+        setModelAndDigest(verticalModel);
+        expect($(element).find('.panel.feedback').length).toBe(0);
+      });
+
       describe('vertical layout', function() {
         it('renders by default', function() {
-          container.elements['1'].setDataAndSession(verticalModel);
-          scope.$digest();
+          setModelAndDigest(verticalModel);
           expect($(element).find('.vertical').length).toBeGreaterThan(0);
           expect($(element).find('.horizontal').length).toBe(0);
         });
 
         it('correct answer and show correct answer button are not visible before submitting', function() {
-          container.elements['1'].setDataAndSession(verticalModel);
-          scope.$digest();
+          setModelAndDigest(verticalModel);
           expect($(element).find('.show-correct-button').hasClass('ng-hide')).toBe(true);
           expect($(element).find('.correct-answer').hasClass('ng-hide')).toBe(true);
         });
 
         it('show correct answer button are visible after submitting', function() {
-          container.elements['1'].setDataAndSession(verticalModel);
-          scope.$digest();
-          container.elements['1'].setResponse({correctness: 'incorrect', correctResponse: ['a','b','c','d']});
+          setModelAndDigest(verticalModel);
+
+          container.elements['1'].setResponse({correctness: 'incorrect', correctResponse: ['a', 'b', 'c', 'd']});
           scope.$digest();
           expect($(element).find('.show-correct-button').hasClass('ng-hide')).toBe(false);
+        });
+
+        it('correct answer is visible after submitting an incorrect answer', function() {
+          setModelAndDigest(horizontalModel);
+          setResponseAndDigest({correctness: 'incorrect', correctClass: 'incorrect', correctResponse: ['a','b','c','d']});
+          expect(element.scope().correctChoices.length).toBe(4);
         });
 
       });
@@ -114,14 +132,71 @@ describe('corespring', function() {
         });
 
         it('show correct answer button are visible after submitting', function() {
-          console.log(horizontalModel);
           container.elements['1'].setDataAndSession(horizontalModel);
           scope.$digest();
-          container.elements['1'].setResponse({correctness: 'incorrect', correctResponse: ['a','b','c','d']});
+          container.elements['1'].setResponse({correctness: 'incorrect', correctResponse: ['a', 'b', 'c', 'd']});
           scope.$digest();
           expect($(element).find('.see-answer-panel').hasClass('ng-hide')).toBe(false);
         });
 
+        it('correct answer is visible after submitting an incorrect answer', function() {
+          setModelAndDigest(horizontalModel);
+          setResponseAndDigest({correctness: 'incorrect', correctClass: 'incorrect', correctResponse: ['a','b','c','d']});
+          expect($(element).find('.see-answer-panel').hasClass('ng-hide')).toBe(false);
+        });
+
+        it('correct answer is not visible after submitting a correct answer', function() {
+          setModelAndDigest(horizontalModel);
+          setResponseAndDigest({correctness: 'correct', correctClass: 'correct', correctResponse: ['a','b','c','d']});
+          expect($(element).find('.see-answer-panel').hasClass('ng-hide')).toBe(true);
+        });
+
+      });
+
+      describe('feedback', function() {
+        it('correct feedback is shown after submitting the correct answer', function() {
+          setModelAndDigest(verticalModel);
+          setResponseAndDigest({correctness: 'correct', correctClass: 'correct', feedback: "Correct"});
+          expect($(element).find('.panel.feedback.correct').length).toBe(1);
+          expect(element.scope().feedback).toBe("Correct");
+        });
+        it('incorrect feedback is shown after submitting an incorrect answer', function() {
+          setModelAndDigest(verticalModel);
+          setResponseAndDigest({correctness: 'incorrect', correctClass: 'incorrect', feedback: "Incorrect"});
+          expect($(element).find('.panel.feedback.incorrect').length).toBe(1);
+          expect(element.scope().feedback).toBe("Incorrect");
+        });
+        it('partial feedback is shown after submitting a partially correct answer', function() {
+          setModelAndDigest(verticalModel);
+          setResponseAndDigest({correctness: 'incorrect', correctClass: 'partial', feedback: "Partial"});
+          expect($(element).find('.panel.feedback.partial').length).toBe(1);
+          expect(element.scope().feedback).toBe("Partial");
+        });
+      });
+
+      describe('evaluate', function() {
+        it('incorrect choices are marked as incorrect', function() {
+          setModelAndDigest(verticalModel);
+          scope.local.choices = [
+            {id: 'a'},
+            {id: 'b'},
+            {id: 'c'},
+            {id: 'd'}
+          ];
+          setResponseAndDigest({correctness: 'incorrect', correctClass: 'partial', feedback: "Partial", correctResponse: ['a', 'c', 'b', 'd']});
+          expect(element.find('.container-border .incorrect').length).toBe(2);
+        });
+        it('correct choices are marked as correct', function() {
+          setModelAndDigest(verticalModel);
+          scope.local.choices = [
+            {id: 'a'},
+            {id: 'b'},
+            {id: 'c'},
+            {id: 'd'}
+          ];
+          setResponseAndDigest({correctness: 'incorrect', correctClass: 'partial', feedback: "Partial", correctResponse: ['a', 'c', 'b', 'd']});
+          expect(element.find('.container-border .correct').length).toBe(2);
+        });
       });
 
     });
