5f892ca857866082e3049cf1af184fffa8613314
Venus Ang <venusang24@gmail.com>
2015-02-05 08:56:59 -0800

    Added 1 x/y to hints.

--- components/corespring/function-entry/src/client/configure.js
+++ components/corespring/function-entry/src/client/configure.js
@@ -105,6 +105,7 @@ var helpPopover = [
           '    <li>For \\( \\frac{2}{x+3} \\), enter \\( 2 / (x+3) \\)</li>',
           '    <li>For \\( x^{y} \\), enter \\( x \\) ^ \\( y \\)</li>',
           '    <li>For \\( x^{2} \\), enter \\( x \\) ^ \\( 2 \\)</li>',
+          '    <li>For \\( 1 \\frac{1}{x} \\), enter \\(1 \\) \\( x/y \\)</li>',        
           '    <li>For \\( \\sqrt{x} \\), enter \sqrt(x)</li>',
           '</ul>'
         ].join('');
