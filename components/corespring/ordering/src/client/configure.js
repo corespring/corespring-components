52b16b93922e6c5464201c93e690dbe6e9535cc0
Attila Miklosi <attila.miklosi@gmail.com>
2015-02-27 16:56:49 +0100

    Ralf happiness

--- components/corespring/ordering/src/client/configure.js
+++ components/corespring/ordering/src/client/configure.js
@@ -190,11 +190,11 @@ var main = [
               initTargets();
             },
             getModel: function() {
-              var model = _.cloneDeep($scope.fullModel);
-              if (model.model.config.placementType === 'inPlace') {
-                model.correctResponse = _.pluck(model.model.choices, 'id');
+              var fullModel = _.cloneDeep($scope.fullModel);
+              if (fullModel.model.config.placementType === 'inPlace') {
+                fullModel.correctResponse = _.pluck(fullModel.model.choices, 'id');
               }
-              return model;
+              return fullModel;
             }
           };
 
