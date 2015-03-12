53c8dd1c4c0b6d3f24de241bed2f3be09e530928
Attila Miklosi <attila.miklosi@gmail.com>
2015-03-12 15:45:06 +0100

    config panel wip

--- components/corespring/number-line/src/client/configure.js
+++ components/corespring/number-line/src/client/configure.js
@@ -1,23 +1,119 @@
 var main = [
-  '$log',
-  function($log) {
+  '$log','ChoiceTemplates',
+  function($log,ChoiceTemplates) {
 
+    var attributes = [
+      '  <h3>Number Line Attributes</h3>',
+      '  <checkbox ng-model="fullModel.model.config.exhibitOnly">Make exhibit</checkbox>',
+      '  <div>',
+      '     Domain = <input type="number" class="form-control fixed-input-100" ng-model="fullModel.model.config.domain[0]"/> to <input type="number" class="form-control fixed-input-100" ng-model="fullModel.model.config.domain[1]"/>',
+      '  </div>',
+      '  <div>Number of Ticks: <input type="number" class="form-control fixed-input-100" ng-model="fullModel.model.config.tickFrequency"/></div>',
+      '  <checkbox ng-model="fullModel.model.config.showMinorTicks">Display minor tick marks</checkbox>',
+      '  <div>Minor tick frequency: <input type="number" class="form-control fixed-input-100" ng-model="fullModel.model.config.snapPerTick"/></div>',
+      '  <div><a class="reset-defaults btn btn-default" ng-click="resetDefaults()">Reset to default values</a></div>',
+    ].join('');
+
+    var initialView = [
+      '<h3>Initial view</h3>',
+      '  <div interactive-graph',
+      '       ngModel="initialView.model"',
+      '       responseModel="initialView.responseModel"',
+      '       editable="initialView.editable"',
+      '       colors="initialView.colors"></div>',
+
+    ].join('');
+
+    var correctResponseView = [
+      '<h3>Correct Response</h3>',
+      '  <div interactive-graph',
+      '       ngModel="correctResponseView.model"',
+      '       responseModel="correctResponseView.responseModel"',
+      '       editable="correctResponseView.editable"></div>',
+
+    ].join('');
 
     return {
       scope: 'isolate',
       restrict: 'E',
       replace: true,
       link: function(scope, element, attrs) {
+        ChoiceTemplates.extendScope(scope, 'corespring-number-line');
+        scope.initialView = {
+          editable: true,
+          model: {
+            "config": {
+              "domain": [0, 20],
+              "maxNumberOfPoints": 3,
+              "tickFrequency": 20,
+              "snapPerTick": 2,
+              "showMinorTicks": true,
+              "initialType": "PF",
+              "exhibitOnly": false,
+              "availableTypes": {
+                "PF": true,
+                "LEE": true,
+                "LEF": true,
+                "LFE": true,
+                "LFF": true,
+                "REP": true,
+                "REN": true,
+                "RFP": true,
+                "RFN": true
+              }
+            }
+          }
+        };
+
+        scope.correctResponseView = _.cloneDeep(scope.initialView);
+        scope.correctResponseView.responseModel = {};
 
         scope.containerBridge = {
           setModel: function(model) {
             scope.fullModel = model;
+            scope.initialView.model.config.initialElements = _.cloneDeep(model.model.config.initialElements);
+            scope.correctResponseView.model.config.initialElements = _.cloneDeep(model.correctResponse);
           },
 
           getModel: function() {
             return scope.fullModel;
           }
         };
+
+        var updateInitialElements = function(n) {
+          scope.$apply(function() {
+            scope.fullModel.model.config.initialElements = _.cloneDeep(n);
+          });
+        };
+
+        var updateCorrectResponse = function(n) {
+          scope.$apply(function() {
+            scope.fullModel.correctResponse = _.cloneDeep(n);
+            scope.updatePartialScoringModel(scope.correctResponseView.responseModel.length);
+          });
+        };
+
+        function debounce(fn) {
+          return _.debounce(fn, 500, {
+            trailing: true,
+            leading: false
+          });
+        }
+
+        scope.$watch('initialView.responseModel', debounce(updateInitialElements), true);
+
+        scope.$watch('correctResponseView.responseModel', debounce(updateCorrectResponse), true);
+
+        scope.$watch('fullModel.model.config', function(n) {
+          if (n) {
+            _(n).omit('initialElements').each(function(e, k) {
+              if (!_.isUndefined(n[k])) {
+                scope.initialView.model.config[k] = n[k];
+                scope.correctResponseView.model.config[k] = n[k];
+              }
+            });
+          }
+        }, true);
 
         // Temporary way to test the interaction
         scope.$watch('fullModel', function(n) {
@@ -26,7 +122,7 @@ var main = [
           }
         }, true);
 
-        scope.$watch("modelJson", _.debounce(function (json) {
+        scope.$watch("modelJson", _.debounce(function(json) {
           if (!_.isUndefined(json)) {
             scope.$apply(function() {
               var newModel = JSON.parse(json);
@@ -41,12 +137,22 @@ var main = [
       },
       template: [
         '<div class="config-number-line">',
-        '  <div>Domain: <input type="number" ng-model="fullModel.model.config.domain[0]"/><input type="number" ng-model="fullModel.model.config.domain[1]"/></div>',
-        '  <div>Max Number of Points: <input type="number" ng-model="fullModel.model.config.maxNumberOfPoints"/></div>',
-        '  <div>Tick Freq: <input type="number" ng-model="fullModel.model.config.tickFrequency"/></div>',
-        '  <div>Snap per Tick: <input type="number" ng-model="fullModel.model.config.snapPerTick"/></div>',
-        '  <div><input type="checkbox" id="group" ng-model="fullModel.model.config.groupingEnabled" /><label for="group">Group types</label></div>',
-        '  <textarea cols="100" rows="50" ng-model="modelJson"></textarea>',
+        '  <div navigator-panel="Design">',
+        '  <p>',
+        '    In Number Line, students identify coordinates or plot points on a graph by clicking on the graph.',
+        '  </p>',
+        attributes,
+        initialView,
+        correctResponseView,
+        '  </div>',
+        '  <div navigator-panel="DevMode">',
+        '    <textarea cols="100" rows="50" ng-model="modelJson"></textarea>',
+        '  </div>',
+        '  <div navigator-panel="Scoring">',
+        ChoiceTemplates.scoring(),
+        '  </div>',
+
+        '</div>',
         '</div>'
       ].join("")
     };
@@ -55,6 +161,8 @@ var main = [
 ];
 
 exports.framework = 'angular';
-exports.directives = [{
-  directive: main
-}];
+exports.directives = [
+  {
+    directive: main
+  }
+];
