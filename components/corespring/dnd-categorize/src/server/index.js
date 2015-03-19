017095b6ee879629d4e2ec7e3dbd3572ab841814
dimabur <dimabur@gmail.com>
2015-03-19 18:02:32 +0200

    Categorize using createOutcome server interface

--- components/corespring/dnd-categorize/src/server/index.js
+++ components/corespring/dnd-categorize/src/server/index.js
@@ -93,7 +93,7 @@ var createResponse = function(question, answer, settings) {
 
 exports.keys = fb.keys;
 
-exports.respond = function(question, answer, settings) {
+exports.createOutcome = function(question, answer, settings) {
 
   var defaults = {
     correct: exports.DEFAULT_CORRECT_FEEDBACK,
