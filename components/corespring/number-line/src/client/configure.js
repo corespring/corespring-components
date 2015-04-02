05bd69b127f37800b25246456aa7b8d5218e3f9b
Attila Miklosi <attila.miklosi@gmail.com>
2015-04-02 20:07:35 +0200

    added text below initial view

--- components/corespring/number-line/src/client/configure.js
+++ components/corespring/number-line/src/client/configure.js
@@ -34,6 +34,9 @@ var main = [
 
     var initialView = [
       '<div collapsable-panel collapsable-panel-title="Initial view / Make Exhibit">',
+      '  <p>Use this number line to set a starting point, line segment or ray. This is optional. ',
+      '  This number line may also be used to make an exhibit number line, which can not be manipulated by a student.',
+      '  Be sure to check “Make an Exhibit” in the Number Line Attributes area above.</p>',
       '  <div interactive-graph',
       '       ngModel="initialView.model"',
       '       options="configGraphOptions"',
