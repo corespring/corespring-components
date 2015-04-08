83f44ec99433d46e1313c2cfe2f19ad1aada4961
Attila Miklosi <attila.miklosi@gmail.com>
2015-04-08 17:57:01 +0200

    allowing collapsable panel to be open by default

--- components/corespring/widgets/src/client/collapsable-panel.js
+++ components/corespring/widgets/src/client/collapsable-panel.js
@@ -7,27 +7,34 @@ exports.directive = {
 
       return {
         link: function($scope, $element, $attrs) {
+          $scope.randomId = Math.floor(Math.random() * 1000);
           $scope.isOpen = false;
           $scope.toggle = function() {
             $scope.isOpen = !$scope.isOpen;
           };
+          $scope.$watch('defaultState', function(n) {
+             if (n) {
+               $scope.isOpen = n === 'in';
+             }
+          });
         },
         replace: true,
         transclude: true,
         scope: {
-          collapsablePanelTitle: "@"
+          collapsablePanelTitle: "@",
+          defaultState: "@collapsablePanelDefaultState"
         },
         template: [
           '<div class="panel panel-default collapsable-panel">',
-          '  <div class="panel-heading" ng-click="toggle()" data-toggle="collapse" href="#collapsePanel">',
+          '  <div class="panel-heading" ng-click="toggle()" data-toggle="collapse" href="#collapsePanel{{randomId}}">',
           '    <h4 class="panel-title">',
-          '      <a  class="">',
+          '      <a class="">',
           '        <span class="icon"><i class="fa fa-{{isOpen ? \'minus\' : \'plus\'}}-circle"></i></span>',
           '        {{collapsablePanelTitle}}',
           '      </a>',
           '    </h4>',
           '  </div>',
-          '  <div id="collapsePanel" class="panel-collapse collapse">',
+          '  <div id="collapsePanel{{randomId}}" class="panel-collapse collapse {{defaultState}}">',
           '    <div class="panel-body" ng-transclude>',
           '    </div>',
           '  </div>',
