ee1d3a43e29590150f38e79291ac48607f63b810
rbokel <rbokel.github@qlod.org>
2015-04-15 15:27:09 +0200

    add unit tests for config

---
+++ components/corespring/match/test/client/configure-test.js
@@ -0,0 +1,213 @@
+describe('corespring:match:configure', function() {
+
+  "use strict";
+
+  var MockComponentRegister = function() {
+    this.elements = {};
+    this.registerConfigPanel = function(id, bridge) {
+      this.elements[id] = bridge;
+    };
+  };
+
+  var element = null,
+    container = null,
+    scope, rootScope;
+
+  function createTestModel() {
+    return {
+      componentType: "corespring-match",
+      title: "Match component sample item",
+      weight: 4,
+      correctResponse: [
+        {
+          id: "row-1",
+          matchSet: [true, false]
+            },
+        {
+          id: "row-2",
+          matchSet: [true, false]
+            },
+        {
+          id: "row-3",
+          matchSet: [true, false]
+            },
+        {
+          id: "row-4",
+          matchSet: [true, false]
+            }
+          ],
+      allowPartialScoring: true,
+      partialScoring: [
+        {numberOfCorrect: 1, scorePercentage: 10},
+        {numberOfCorrect: 2, scorePercentage: 20},
+        {numberOfCorrect: 3, scorePercentage: 30},
+        {numberOfCorrect: 4, scorePercentage: 40}
+      ],
+      feedback: {
+        correctFeedbackType: "default",
+        partialFeedbackType: "default",
+        incorrectFeedbackType: "custom",
+        incorrectFeedback: " <span mathjax=\"\">\\(\\frac12\\)</span> Everything is wrong !"
+      },
+      model: {
+        columns: [
+          {
+            labelHtml: "Custom header"
+              },
+          {
+            labelHtml: "Column 1"
+              },
+          {
+            labelHtml: "Column 2"
+              }
+            ],
+        rows: [
+          {
+            id: "row-1",
+            labelHtml: "Question text 1"
+              },
+          {
+            id: "row-2",
+            labelHtml: "Question text 2"
+              },
+          {
+            id: "row-3",
+            labelHtml: "Question text 3"
+              },
+          {
+            id: "row-4",
+            labelHtml: "Question text 4"
+              }
+            ],
+        config: {
+          inputType: 'radiobutton',
+          shuffle: false,
+          layout: 'three-columns'
+        }
+      }
+    };
+  }
+
+  beforeEach(angular.mock.module('test-app'));
+
+  var MockServerLogic = {
+    load: function() {
+      return {
+        defaults: {},
+        keys: {}
+      };
+    }
+  };
+
+  function MockImageUtils() {}
+
+  function MockWiggiMathJaxFeatureDef() {}
+
+  beforeEach(function() {
+    module(function($provide) {
+      $provide.value('ServerLogic', MockServerLogic);
+      $provide.value('ImageUtils', MockImageUtils);
+      $provide.value('WiggiMathJaxFeatureDef', MockWiggiMathJaxFeatureDef);
+      $provide.value('WiggiLinkFeatureDef', function(){});
+      $provide.value('LogFactory', {
+        getLogger: function() {
+          return {
+            trace: function() {},
+            log: function() {},
+            debug: function() {},
+            warn: function() {},
+            error: function() {},
+            fatal: function() {},
+            info: function() {},
+          };
+        }
+      });
+    });
+  });
+
+  beforeEach(inject(function($compile, $rootScope) {
+    scope = $rootScope.$new();
+    container = new MockComponentRegister();
+
+    $rootScope.$on('registerConfigPanel', function(ev, id, b) {
+      container.registerConfigPanel(id, b);
+    });
+
+    $rootScope.registerConfigPanel = function(id, b) {
+      container.registerConfigPanel(id, b);
+    };
+    element = $compile("<div navigator=''><corespring-match-configure id='1'></corespring-match-configure></div>")(scope);
+    scope = element.scope().$$childHead.$$childHead;
+    rootScope = $rootScope;
+  }));
+
+  it('constructs', function() {
+    expect(element).toNotBe(null);
+  });
+
+  it('component is being registered by the container', function() {
+    expect(container.elements['1']).toBeDefined();
+    expect(container.elements['2']).toBeUndefined();
+  });
+
+  describe('partialScoring', function() {
+    it('should automatically remove additional partial scoring scenarios after removing a correct choice', function() {
+      var testModel = createTestModel();
+      container.elements['1'].setModel(testModel);
+      rootScope.$digest();
+      expect(scope.fullModel.partialScoring.length).toEqual(3);
+      scope.removeRow(0);
+      rootScope.$digest();
+      expect(scope.fullModel.partialScoring.length).toEqual(2);
+    });
+  });
+
+  describe('addRow', function() {
+    beforeEach(function() {
+      var testModel = createTestModel();
+      container.elements['1'].setModel(testModel);
+    });
+
+    it('should have four rows initially', function() {
+      expect(scope.model.rows.length).toEqual(4);
+    });
+
+    it('should have five rows after addRow', function() {
+      scope.addRow();
+      expect(scope.model.rows.length).toEqual(5);
+    });
+
+    it('the new row should not have something selected', function() {
+      scope.addRow();
+      var matchSet = scope.fullModel.correctResponse[4].matchSet;
+      expect(matchSet).toEqual([false,false]);
+    });
+
+  });
+
+  describe('removeRow', function() {
+    beforeEach(function() {
+      var testModel = createTestModel();
+      container.elements['1'].setModel(testModel);
+    });
+
+    it('should have four rows initially', function() {
+      expect(scope.model.rows.length).toEqual(4);
+    });
+
+    it('should have three rows after removeRow', function() {
+      scope.removeRow(0);
+      expect(scope.model.rows.length).toEqual(3);
+    });
+
+    it('the correctResponse for this row should also be removed', function() {
+      var rowOne = _.find(scope.fullModel.correctResponse, {id:'row-1'});
+      expect(rowOne).toBeDefined();
+      scope.removeRow(0);
+      rowOne = _.find(scope.fullModel.correctResponse, {id:'row-1'});
+      expect(rowOne).toBeUndefined();
+    });
+
+  });
+
+});
--- components/corespring/match/test/client/render-test.js
+++ components/corespring/match/test/client/render-test.js
@@ -1,267 +1,278 @@
-describe('corespring', function() {
+describe('corespring:match:render', function() {
 
-  describe('match render', function() {
-
-    var MockComponentRegister = function() {
-      this.elements = {};
-      this.registerComponent = function(id, bridge) {
-        this.elements[id] = bridge;
-      };
+  var MockComponentRegister = function() {
+    this.elements = {};
+    this.registerComponent = function(id, bridge) {
+      this.elements[id] = bridge;
     };
+  };
 
-    var element, scope, rootScope, container;
+  var element, scope, rootScope, container;
 
-    var testModel;
+  var testModel;
 
-    var testModelTemplate = {
-      data: {
-        "model": {
-          "columns": [
-            {
-              "labelHtml": "Custom header"
+  var testModelTemplate = {
+    data: {
+      "model": {
+        "columns": [
+          {
+            "labelHtml": "Custom header"
             },
-            {
-              "labelHtml": "Column 1"
+          {
+            "labelHtml": "Column 1"
             },
-            {
-              "labelHtml": "Column 2"
+          {
+            "labelHtml": "Column 2"
             }
           ],
-          "rows": [
-            {
-              "id": "1",
-              "labelHtml": "Question text 1"
+        "rows": [
+          {
+            "id": "1",
+            "labelHtml": "Question text 1"
             },
-            {
-              "id": "2",
-              "labelHtml": "Question text 2"
+          {
+            "id": "2",
+            "labelHtml": "Question text 2"
             },
-            {
-              "id": "3",
-              "labelHtml": "Question text 3"
+          {
+            "id": "3",
+            "labelHtml": "Question text 3"
             },
-            {
-              "id": "4",
-              "labelHtml": "Question text 4"
+          {
+            "id": "4",
+            "labelHtml": "Question text 4"
             }
           ],
-          "answerType": "YES_NO"
-        }
+        "answerType": "YES_NO"
       }
-    };
+    }
+  };
 
-    beforeEach(angular.mock.module('test-app'));
+  beforeEach(angular.mock.module('test-app'));
 
-    beforeEach(function() {
-      module(function($provide) {
-        testModel = _.cloneDeep(testModelTemplate);
-      });
+  beforeEach(function() {
+    module(function($provide) {
+      testModel = _.cloneDeep(testModelTemplate);
+    });
+  });
+
+  beforeEach(inject(function($compile, $rootScope) {
+    container = new MockComponentRegister();
+
+    $rootScope.$on('registerComponent', function(event, id, obj) {
+      console.log('registerComponent');
+      container.registerComponent(id, obj);
     });
 
-    beforeEach(inject(function($compile, $rootScope) {
-      container = new MockComponentRegister();
+    element = $compile("<corespring-match-render id='1'></corespring-match-render>")($rootScope.$new());
+    scope = element.scope().$$childHead;
+    rootScope = $rootScope;
+  }));
 
-      $rootScope.$on('registerComponent', function(event, id, obj) {
-        console.log('registerComponent');
-        container.registerComponent(id, obj);
-      });
+  it('constructs', function() {
+    expect(element).toNotBe(null);
+  });
 
-      element = $compile("<corespring-match-render id='1'></corespring-match-render>")($rootScope.$new());
-      scope = element.scope().$$childHead;
-      rootScope = $rootScope;
-    }));
+  it('sets model', function() {
+    container.elements['1'].setDataAndSession(testModel);
+    expect(scope.question).toNotBe(null);
+    expect(scope.inputType).toBe('radiobutton');
+  });
 
-    it('constructs', function() {
-      expect(element).toNotBe(null);
-    });
+  it('uses the column header if available', function() {
+    container.elements['1'].setDataAndSession(testModel);
+    expect(scope.matchModel.columns[1].labelHtml).toEqual('Column 1');
+    expect(scope.matchModel.columns[2].labelHtml).toEqual('Column 2');
+  });
 
-    it('sets model', function() {
-      container.elements['1'].setDataAndSession(testModel);
-      expect(scope.question).toNotBe(null);
-      expect(scope.inputType).toBe('radiobutton');
-    });
+  it('uses the default header if column header is empty', function() {
+    testModel.data.model.columns[1].labelHtml = null;
+    testModel.data.model.columns[2].labelHtml = '';
+    container.elements['1'].setDataAndSession(testModel);
+    expect(scope.matchModel.columns[1].labelHtml).toEqual('Yes');
+    expect(scope.matchModel.columns[2].labelHtml).toEqual('No');
+  });
 
-    it('uses the column header if available', function(){
-      container.elements['1'].setDataAndSession(testModel);
-      expect(scope.matchModel.columns[1].labelHtml).toEqual('Column 1');
-      expect(scope.matchModel.columns[2].labelHtml).toEqual('Column 2');
-    });
+  it('uses the default header if column does not exist', function() {
+    testModel.data.model.columns = [];
+    container.elements['1'].setDataAndSession(testModel);
+    expect(scope.matchModel.columns[1].labelHtml).toEqual('Yes');
+    expect(scope.matchModel.columns[2].labelHtml).toEqual('No');
+  });
 
-    it('uses the default header if column header is empty', function(){
-      testModel.data.model.columns[1].labelHtml = null;
-      testModel.data.model.columns[2].labelHtml = '';
-      container.elements['1'].setDataAndSession(testModel);
-      expect(scope.matchModel.columns[1].labelHtml).toEqual('Yes');
-      expect(scope.matchModel.columns[2].labelHtml).toEqual('No');
-    });
+  it('builds the table correctly', function() {
+    container.elements['1'].setDataAndSession(testModel);
+    rootScope.$digest();
+    var table = $(element).find('table');
+    expect(table.length).toBe(1);
+    expect(table.find('td').length).toBe(16);
+  });
 
-    it('uses the default header if column does not exist', function(){
-      testModel.data.model.columns = [];
-      container.elements['1'].setDataAndSession(testModel);
-      expect(scope.matchModel.columns[1].labelHtml).toEqual('Yes');
-      expect(scope.matchModel.columns[2].labelHtml).toEqual('No');
-    });
+  it('selects correctly radio buttons', function() {
+    container.elements['1'].setDataAndSession(testModel);
+    rootScope.$digest();
+    var row = scope.matchModel.rows[0];
+    var matchSet = row.matchSet;
+    scope.onClickMatch(row, 0);
+    rootScope.$digest();
+    expect(matchSet[0].value).toBe(true);
+    expect(matchSet[1].value).toBe(false);
 
-    it('builds the table correctly', function() {
-      container.elements['1'].setDataAndSession(testModel);
-      rootScope.$digest();
-      var table = $(element).find('table');
-      expect(table.length).toBe(1);
-      expect(table.find('td').length).toBe(16);
-    });
+    row = scope.matchModel.rows[1];
+    scope.onClickMatch(row, 1);
+    matchSet = row.matchSet;
+    rootScope.$digest();
+    expect(matchSet[0].value).toBe(false);
+    expect(matchSet[1].value).toBe(true);
+  });
 
-    it('selects correctly radio buttons', function() {
+  describe('tag filtering for labels', function() {
+    it('removes size settings', function() {
+      testModel.data.model.rows[0].labelHtml = '<div width="1px" height="2px" min-width="3px" min-height="4px" style="width:5px; min-width:6px; height:7px; min-height:8px; ">some text</div>';
       container.elements['1'].setDataAndSession(testModel);
       rootScope.$digest();
       var row = scope.matchModel.rows[0];
-      var matchSet = row.matchSet;
-      scope.onClickMatch(row,0);
-      rootScope.$digest();
-      expect(matchSet[0].value).toBe(true);
-      expect(matchSet[1].value).toBe(false);
-
-      row = scope.matchModel.rows[1];
-      scope.onClickMatch(row,1);
-      matchSet = row.matchSet;
-      rootScope.$digest();
-      expect(matchSet[0].value).toBe(false);
-      expect(matchSet[1].value).toBe(true);
+      expect(row.labelHtml).toEqual('<div style="">some text</div>');
     });
 
-    describe('tag filtering for labels',function(){
-      it('removes size settings', function(){
-        testModel.data.model.rows[0].labelHtml = '<div width="1px" height="2px" min-width="3px" min-height="4px" style="width:5px; min-width:6px; height:7px; min-height:8px; ">some text</div>';
-        container.elements['1'].setDataAndSession(testModel);
-        rootScope.$digest();
-        var row = scope.matchModel.rows[0];
-        expect(row.labelHtml).toEqual('<div style="">some text</div>');
-      });
-
-      it('does not remove other settings', function(){
-        testModel.data.model.rows[0].labelHtml = '<div class="someClass" style="border:none;">some text</div>';
-        container.elements['1'].setDataAndSession(testModel);
-        rootScope.$digest();
-        var row = scope.matchModel.rows[0];
-        expect(row.labelHtml).toEqual('<div class="someClass" style="border:none;">some text</div>');
-      });
+    it('does not remove other settings', function() {
+      testModel.data.model.rows[0].labelHtml = '<div class="someClass" style="border:none;">some text</div>';
+      container.elements['1'].setDataAndSession(testModel);
+      rootScope.$digest();
+      var row = scope.matchModel.rows[0];
+      expect(row.labelHtml).toEqual('<div class="someClass" style="border:none;">some text</div>');
     });
+  });
 
-    it('returns session correctly', function() {
-      var component =  container.elements['1'];
+  it('returns session correctly', function() {
+    var component = container.elements['1'];
+    component.setDataAndSession(testModel);
+    rootScope.$digest();
+
+    var row = scope.matchModel.rows[0];
+    scope.onClickMatch(row, 0);
+    rootScope.$digest();
+
+    var session = component.getSession();
+
+    expect(session.answers.length).toBe(testModel.data.model.rows.length);
+    expect(session.answers[0].id).toBe(testModel.data.model.rows[0].id);
+    expect(session.answers[0].matchSet[0]).toBe(true);
+    expect(session.answers[0].matchSet[1]).toBe(false);
+  });
+
+  describe('config', function() {
+
+    function setModel(answerType, config, columns) {
+      testModel.data.model.answerType = answerType;
+      testModel.data.model.config = config;
+      if (columns) {
+        testModel.data.model.columns = columns;
+      }
+      var component = container.elements['1'];
       component.setDataAndSession(testModel);
-      rootScope.$digest();
+    }
 
-      var row = scope.matchModel.rows[0];
-      scope.onClickMatch(row,0);
-      rootScope.$digest();
-
-      var session = component.getSession();
-
-      expect(session.answers.length).toBe(testModel.data.model.rows.length);
-      expect(session.answers[0].id).toBe(testModel.data.model.rows[0].id);
-      expect(session.answers[0].matchSet[0]).toBe(true);
-      expect(session.answers[0].matchSet[1]).toBe(false);
+    describe("sets inputType radiobutton", function() {
+      it('if answerType is "YES_NO"', function() {
+        setModel('YES_NO');
+        expect(scope.inputType).toBe('radiobutton');
+      });
+      it('if answerType is "TRUE_FALSE"', function() {
+        setModel('TRUE_FALSE');
+        expect(scope.inputType).toBe('radiobutton');
+      });
+      it('if answerType is undefined', function() {
+        setModel(undefined);
+        expect(scope.inputType).toBe('radiobutton');
+      });
+      it('if config.inputType is Radio', function() {
+        setModel(undefined, {
+          inputType: 'Radio'
+        });
+        expect(scope.inputType).toBe('radiobutton');
+      });
     });
 
-    describe('config', function(){
-
-      function setModel(answerType, config, columns){
-        testModel.data.model.answerType = answerType;
-        testModel.data.model.config = config;
-        if(columns){
-          testModel.data.model.columns = columns;
-        }
-        var component =  container.elements['1'];
-        component.setDataAndSession(testModel);
-      }
-
-      describe("sets inputType radiobutton", function(){
-        it('if answerType is "YES_NO"', function(){
-          setModel('YES_NO');
-          expect(scope.inputType).toBe('radiobutton');
-        });
-        it('if answerType is "TRUE_FALSE"', function(){
-          setModel('TRUE_FALSE');
-          expect(scope.inputType).toBe('radiobutton');
-        });
-        it('if answerType is undefined', function(){
-          setModel(undefined);
-          expect(scope.inputType).toBe('radiobutton');
-        });
-        it('if config.inputType is Radio', function(){
-          setModel(undefined, {inputType:'Radio'});
-          expect(scope.inputType).toBe('radiobutton');
-        });
+    describe("sets inputType checkbox", function() {
+      it('if answerType is "MULTIPLE"', function() {
+        setModel('MULTIPLE');
+        expect(scope.inputType).toBe('checkbox');
       });
-
-      describe("sets inputType checkbox", function(){
-        it('if answerType is "MULTIPLE"', function(){
-          setModel('MULTIPLE');
-          expect(scope.inputType).toBe('checkbox');
+      it('if config.inputType is Checkbox', function() {
+        setModel(undefined, {
+          inputType: 'Checkbox'
         });
-        it('if config.inputType is Checkbox', function(){
-          setModel(undefined, {inputType:'Checkbox'});
-          expect(scope.inputType).toBe('checkbox');
-        });
+        expect(scope.inputType).toBe('checkbox');
       });
+    });
 
-      describe("sets layout three-columns", function(){
-        it('if config is "3 Columns"', function(){
-          setModel(undefined, {layout: '3 Columns'});
-          expect(scope.layout).toBe('three-columns');
+    describe("sets layout three-columns", function() {
+      it('if config is "3 Columns"', function() {
+        setModel(undefined, {
+          layout: '3 Columns'
         });
-        it('if model has three columns', function(){
-          setModel(undefined, undefined, [{}, {}, {}]);
-          expect(scope.layout).toBe('three-columns');
-        });
-        it('if model has less than three columns', function(){
-          setModel(undefined, undefined, [{}]);
-          expect(scope.layout).toBe('three-columns');
-        });
+        expect(scope.layout).toBe('three-columns');
       });
-
-      describe("sets layout four-columns", function(){
-        it('if config is "4 Columns"', function(){
-          setModel(undefined, {layout: '4 Columns'});
-          expect(scope.layout).toBe('four-columns');
-        });
-        it('if model has four columns', function(){
-          setModel(undefined, undefined, [{}, {}, {}, {}]);
-          expect(scope.layout).toBe('four-columns');
-        });
+      it('if model has three columns', function() {
+        setModel(undefined, undefined, [{}, {}, {}]);
+        expect(scope.layout).toBe('three-columns');
       });
-
-      describe("sets layout five-columns", function(){
-        it('if config is "5 Columns"', function(){
-          setModel(undefined, {layout: '5 Columns'});
-          expect(scope.layout).toBe('five-columns');
-        });
-        it('if model has five columns', function(){
-          setModel(undefined, undefined, [{}, {}, {}, {}, {}]);
-          expect(scope.layout).toBe('five-columns');
-        });
-        it('if model has more than five columns', function(){
-          setModel(undefined, undefined, [{}, {}, {}, {}, {}, {}]);
-          expect(scope.layout).toBe('five-columns');
-        });
+      it('if model has less than three columns', function() {
+        setModel(undefined, undefined, [{}]);
+        expect(scope.layout).toBe('three-columns');
       });
+    });
 
-      describe('sets shuffle', function(){
-        it('to false if config undefined', function(){
-          setModel(undefined, undefined);
-          expect(scope.shuffle).toBe(false);
+    describe("sets layout four-columns", function() {
+      it('if config is "4 Columns"', function() {
+        setModel(undefined, {
+          layout: '4 Columns'
         });
-        it('to false if config is defined and config.shuffle is false', function(){
-          setModel(undefined, {shuffle: false});
-          expect(scope.shuffle).toBe(false);
-        });
-        it('to true if config is defined and config.shuffle is true', function(){
-          setModel(undefined, {shuffle: true});
-          expect(scope.shuffle).toBe(true);
-        });
+        expect(scope.layout).toBe('four-columns');
       });
+      it('if model has four columns', function() {
+        setModel(undefined, undefined, [{}, {}, {}, {}]);
+        expect(scope.layout).toBe('four-columns');
+      });
+    });
 
+    describe("sets layout five-columns", function() {
+      it('if config is "5 Columns"', function() {
+        setModel(undefined, {
+          layout: '5 Columns'
+        });
+        expect(scope.layout).toBe('five-columns');
+      });
+      it('if model has five columns', function() {
+        setModel(undefined, undefined, [{}, {}, {}, {}, {}]);
+        expect(scope.layout).toBe('five-columns');
+      });
+      it('if model has more than five columns', function() {
+        setModel(undefined, undefined, [{}, {}, {}, {}, {}, {}]);
+        expect(scope.layout).toBe('five-columns');
+      });
+    });
+
+    describe('sets shuffle', function() {
+      it('to false if config undefined', function() {
+        setModel(undefined, undefined);
+        expect(scope.shuffle).toBe(false);
+      });
+      it('to false if config is defined and config.shuffle is false', function() {
+        setModel(undefined, {
+          shuffle: false
+        });
+        expect(scope.shuffle).toBe(false);
+      });
+      it('to true if config is defined and config.shuffle is true', function() {
+        setModel(undefined, {
+          shuffle: true
+        });
+        expect(scope.shuffle).toBe(true);
+      });
     });
 
   });
+
 });
