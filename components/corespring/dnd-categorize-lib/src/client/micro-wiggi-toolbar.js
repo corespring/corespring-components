2fbf01dcc386a7f4608fd90c648cbb5044541b51
rbokel <rbokel.github@qlod.org>
2015-05-14 15:30:21 +0200

    CO-101 avoid exceptions in tests

--- components/corespring/dnd-categorize-lib/src/client/micro-wiggi-toolbar.js
+++ components/corespring/dnd-categorize-lib/src/client/micro-wiggi-toolbar.js
@@ -1,8 +1,22 @@
-angular.module('corespring.wiggi-wiz').directive('microWiggiToolbar', [
-  'ToolbarDef',
-  function(ToolbarDef) {
-    return new ToolbarDef({
-      container: 'microWiggiWiz'
-    });
+/**
+ * A local derivate of mini wiggi's toolbar that is needed to be able to derive a wiggi
+ */
+
+(function () {
+  var module;
+
+  try {
+    module = angular.module('corespring.wiggi-wiz');
   }
-]);
+  catch(err){
+    module = angular.module('corespring.wiggi-wiz', []);
+  }
+  module.directive('microWiggiToolbar', [
+    'ToolbarDef',
+    function (ToolbarDef) {
+      return new ToolbarDef({
+        container: 'microWiggiWiz'
+      });
+    }
+  ]);
+})();
--- components/corespring/dnd-categorize-lib/src/client/micro-wiggi-wiz.js
+++ components/corespring/dnd-categorize-lib/src/client/micro-wiggi-wiz.js
@@ -1,105 +1,117 @@
-angular.module('corespring.wiggi-wiz').directive('microWiggiWiz', [
-  'WiggiWizController',
-  'WiggiWizDef',
-  function(WiggiWizController, WiggiWizDef) {
+/**
+ * A local derivate of mini wiggi without automatic activation/deactivation
+ */
 
-    /**
-     * A mini wiggi without automatic activation/deactivation
-     */
-    var def = new WiggiWizDef(template, link, true);
+(function () {
+  var module;
 
-    return {
-      controller: new WiggiWizController(),
-      link: def.link,
-      replace: true,
-      require: 'ngModel',
-      restrict: 'A',
-      scope: {
-        active: '=?',
-        dialogLauncher: '@',
-        externalFeatures: '&features',
-        featureOverrides: '&',
-        formattingDisplay: '@',
-        imageService: '&',
-        onEditorClick: '&',
-        parentSelector: '@'
-      }
-    };
-
-    function link(scope, $element) {
-
-      scope.originalBottomPadding = parseInt($element.css('padding-bottom'), 10);
-      scope.lineHeight = 22;
-
-      scope.active = scope.active === true;
-
-      $element.on('keyup', adjustHeight);
-      $element.on('keydown', adjustHeight);
-
-      scope.$watch('active', onChangeActive);
-
-
-      //---------------------------------
-
-      function onChangeActive(newValue, oldValue) {
-        if(newValue === oldValue){
-          return;
-        }
-        if (newValue) {
-          adjustHeight();
-          if (scope.focusCaretAtEnd) {
-            scope.focusCaretAtEnd();
-          }
-        } else {
-          shrink();
-        }
-      }
-
-      /**
-       * Adds padding to the bottom of the wiggi-wiz-editable div if there is only
-       * one "line" of text, or sets the bottom padding to the original value if
-       * there is > 1 "line" of text.
-       */
-      function adjustHeight() {
-        // TODO: This will become unreliable when we have images
-        function lineCount() {
-          var lineHeight = parseInt($textField().css('font-size') + 2, 10);
-          var overallHeight = parseInt($textField().height(), 10);
-          return Math.round(overallHeight / lineHeight);
-        }
-
-        if (lineCount() === 1) {
-          $textField().css({
-            'padding-bottom': scope.originalBottomPadding + scope.lineHeight + 'px'
-          });
-        } else {
-          shrink();
-        }
-      }
-
-      function $textField() {
-        return $('.wiggi-wiz-editable', $element);
-      }
-
-      function shrink() {
-        $textField().css({
-          'padding-bottom': scope.originalBottomPadding + 'px'
-        });
-      }
-    }
-
-    function template(config) {
-      var placeholder = config.placeholder;
-      return [
-        '<div class="mini-wiggi-wiz" ng-class="{active: active}">',
-        '  <div class="wiggi-wiz-editable"',
-        (placeholder ? 'placeholder="' + placeholder + '"' : ''),
-        '    contenteditable="true"></div>',
-        '  <div class="toolbar">',
-        '    <micro-wiggi-toolbar></micro-wiggi-toolbar>',
-        '  </div>',
-        '</div>'
-      ].join('\n');
-    }
+  try {
+    module = angular.module('corespring.wiggi-wiz');
   }
-]);
+  catch (err) {
+    module = angular.module('corespring.wiggi-wiz', []);
+  }
+
+  module.directive('microWiggiWiz', [
+    'WiggiWizController',
+    'WiggiWizDef',
+    function (WiggiWizController, WiggiWizDef) {
+
+      var def = new WiggiWizDef(template, link, true);
+
+      return {
+        controller: new WiggiWizController(),
+        link: def.link,
+        replace: true,
+        require: 'ngModel',
+        restrict: 'A',
+        scope: {
+          active: '=?',
+          dialogLauncher: '@',
+          externalFeatures: '&features',
+          featureOverrides: '&',
+          formattingDisplay: '@',
+          imageService: '&',
+          onEditorClick: '&',
+          parentSelector: '@'
+        }
+      };
+
+      function link(scope, $element) {
+
+        scope.originalBottomPadding = parseInt($element.css('padding-bottom'), 10);
+        scope.lineHeight = 22;
+
+        scope.active = scope.active === true;
+
+        $element.on('keyup', adjustHeight);
+        $element.on('keydown', adjustHeight);
+
+        scope.$watch('active', onChangeActive);
+
+
+        //---------------------------------
+
+        function onChangeActive(newValue, oldValue) {
+          if (newValue === oldValue) {
+            return;
+          }
+          if (newValue) {
+            adjustHeight();
+            if (scope.focusCaretAtEnd) {
+              scope.focusCaretAtEnd();
+            }
+          } else {
+            shrink();
+          }
+        }
+
+        /**
+         * Adds padding to the bottom of the wiggi-wiz-editable div if there is only
+         * one "line" of text, or sets the bottom padding to the original value if
+         * there is > 1 "line" of text.
+         */
+        function adjustHeight() {
+          // TODO: This will become unreliable when we have images
+          function lineCount() {
+            var lineHeight = parseInt($textField().css('font-size') + 2, 10);
+            var overallHeight = parseInt($textField().height(), 10);
+            return Math.round(overallHeight / lineHeight);
+          }
+
+          if (lineCount() === 1) {
+            $textField().css({
+              'padding-bottom': scope.originalBottomPadding + scope.lineHeight + 'px'
+            });
+          } else {
+            shrink();
+          }
+        }
+
+        function $textField() {
+          return $('.wiggi-wiz-editable', $element);
+        }
+
+        function shrink() {
+          $textField().css({
+            'padding-bottom': scope.originalBottomPadding + 'px'
+          });
+        }
+      }
+
+      function template(config) {
+        var placeholder = config.placeholder;
+        return [
+          '<div class="mini-wiggi-wiz" ng-class="{active: active}">',
+          '  <div class="wiggi-wiz-editable"',
+          (placeholder ? 'placeholder="' + placeholder + '"' : ''),
+          '    contenteditable="true"></div>',
+          '  <div class="toolbar">',
+          '    <micro-wiggi-toolbar></micro-wiggi-toolbar>',
+          '  </div>',
+          '</div>'
+        ].join('\n');
+      }
+    }
+  ]);
+})();
