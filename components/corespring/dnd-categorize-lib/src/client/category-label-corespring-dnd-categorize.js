41edca98282808b9467e9c3344c26766b5cf52fb
rbokel <rbokel.github@qlod.org>
2015-06-03 14:10:56 +0200

    CO-101 change categories layout to use bottom for label and top for choices

---
+++ components/corespring/dnd-categorize-lib/src/client/category-label-corespring-dnd-categorize.js
@@ -0,0 +1,107 @@
+exports.framework = 'angular';
+exports.directive = {
+  name: "categoryLabelCorespringDndCategorize",
+  directive: [
+    'MiniWiggiScopeExtension',
+    CategoryLabelCorespringDndCategorize]
+};
+
+function CategoryLabelCorespringDndCategorize(
+  MiniWiggiScopeExtension
+) {
+
+  return {
+    restrict: 'A',
+    replace: true,
+    controller: ['$scope', controller],
+    link: link,
+    template: template(),
+    scope: {
+      category: '=',
+      isEditMode: '=?editMode',
+      notifyDeleteClicked: '&onDeleteClicked',
+      notifyEditClicked: '&onEditClicked'
+    }
+  };
+
+
+  function controller($scope) {
+    new MiniWiggiScopeExtension().postLink($scope);
+  }
+
+  function link(scope, elem, attrs) {
+
+    var log = console.log.bind(console, '[category-label]');
+
+    scope.active = false;
+    scope.choiceEditMode = scope.isEditMode ? 'delete' : '';
+    scope.showTools = scope.isEditMode;
+
+    scope.onDeleteClicked = onDeleteClicked;
+    scope.onLabelEditClicked = onLabelEditClicked;
+
+    scope.$on('activate', function(event, id) {
+      scope.active = id === getCategoryId();
+    });
+
+    //---------------------------------------------------------------
+
+    function getCategoryId() {
+      return scope.category ? scope.category.model.id : '';
+    }
+
+    function onDeleteClicked() {
+      scope.$$postDigest(function() {
+        scope.notifyDeleteClicked({
+          categoryId: getCategoryId()
+        });
+      });
+    }
+
+    function onLabelEditClicked(event) {
+      event.stopPropagation();
+      scope.notifyEditClicked({
+        categoryId: getCategoryId()
+      });
+    }
+  }
+
+  function template() {
+    return [
+        '<div class="category category-label">',
+        '  <div class="border">',
+        '    <div ng-click="onLabelEditClicked($event)" ng-if="isEditMode">',
+        '      <div class="editor" ',
+        '         active="active"',
+        '         dialog-launcher="external" ',
+        '         disable-auto-activation="true"  ',
+        '         feature-overrides="overrideFeatures"',
+        '         features="extraFeatures" ',
+        '         image-service="imageService()" ',
+        '         mini-wiggi-wiz="" ',
+        '         ng-model="category.model.label" ',
+        '         placeholder="Enter a label"',
+        '      ></div>',
+        '    </div>',
+        '    <div class="html-wrapper" ng-bind-html-unsafe="category.model.label" ng-if="!isEditMode"></div>',
+        '    <ul class="edit-controls" ng-if="showTools">',
+               deleteTool(),
+        '    </ul>',
+        '  </div>',
+        '</td>'
+      ].join('');
+  }
+
+  function deleteTool() {
+    return [
+        '<li class="delete-icon-button" ',
+        '    ng-click="onDeleteClicked()" ' +
+        '    tooltip="delete" ',
+        '    tooltip-append-to-body="true" ',
+        '    tooltip-placement="bottom">',
+        '  <i class="fa fa-trash-o"></i>',
+        '</li>'
+      ].join('');
+  }
+
+}
