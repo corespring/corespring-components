7560062dff3fcc6471259b18b11ff24295a027f8
rbokel <rbokel.github@qlod.org>
2015-06-19 15:34:27 +0200

    CO-239 added tests for removeAnswerArea

--- components/corespring/drag-and-drop-inline/test/client/configure-test.js
+++ components/corespring/drag-and-drop-inline/test/client/configure-test.js
@@ -210,6 +210,33 @@ describe('corespring:drag-and-drop-inline:configure', function() {
     });
   });
 
+  describe('removeAnswerArea', function() {
+    it('should remove answerArea', function() {
+      var ids, testModel = createTestModel({
+        answerAreas: [{
+          "id": "aa_1"
+        }]
+      });
+      container.elements['1'].setModel(testModel);
+      scope.removeAnswerArea('aa_1');
+      expect(testModel.model.answerAreas).toEqual([]);
+    });
+    it('should remove correctResponses for this answerArea', function() {
+      var ids, testModel = createTestModel({
+        answerAreas: [{
+          "id": "aa_1"
+        }],
+        correctResponse: {
+          "aa_1" : ["c1"]
+        }
+      });
+      container.elements['1'].setModel(testModel);
+      scope.removeAnswerArea('aa_1');
+      scope.$digest();
+      expect(testModel.correctResponse).toEqual({});
+    });
+  });
+
   describe('removeSuperfluousAnswerAreaModels', function(){
     it('should remove a model if it does not have a component in xhtml', function(){
       var testModel = createTestModel({
