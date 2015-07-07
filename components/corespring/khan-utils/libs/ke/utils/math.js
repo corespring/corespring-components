4cb0445aa92bfa400bdfc4d4fde9c104d0d53cf8
Arturo Campos <arturo.campos@edify.cr>
2015-07-07 12:16:24 -0600

    Updating perseus lib and removing unnecessary parts

--- components/corespring/khan-utils/libs/ke/utils/math.js
+++ components/corespring/khan-utils/libs/ke/utils/math.js
@@ -10,8 +10,7 @@ $.extend(KhanUtil, {
     // Simplify formulas before display
     cleanMath: function(expr) {
         return typeof expr === "string" ?
-            KhanUtil.tmpl.cleanHTML(expr)
-                .replace(/\+\s*-/g, "- ")
+            expr.replace(/\+\s*-/g, "- ")
                 .replace(/-\s*-/g, "+ ")
                 .replace(/\^1/g, "") :
             expr;
@@ -90,10 +89,10 @@ $.extend(KhanUtil, {
         return digits;
     },
 
-    placesLeftOfDecimal: [$._("one"), $._("ten"), $._("hundred"),
-        $._("thousand")],
-    placesRightOfDecimal: [$._("one"), $._("tenth"), $._("hundredth"),
-        $._("thousandth"),$._("ten thousandth")],
+    placesLeftOfDecimal: ["one", "ten", "hundred",
+        "thousand"],
+    placesRightOfDecimal: ["one", "tenth", "hundredth",
+        "thousandth","ten thousandth"],
 
     powerToPlace: function(power) {
         if (power < 0) {
@@ -110,6 +109,17 @@ $.extend(KhanUtil, {
             return Math.ceil(x - 0.001);
         }
         return Math.floor(x + 0.001);
+    },
+
+    // Bound a number by 1e-6 and 1e20 to avoid exponents after toString
+    bound: function(num) {
+        if (num === 0) {
+            return num;
+        } else if (num < 0) {
+            return -KhanUtil.bound(-num);
+        } else {
+            return Math.max(1e-6, Math.min(num, 1e20));
+        }
     },
 
     factorial: function(x) {
@@ -526,6 +536,73 @@ $.extend(KhanUtil, {
         }
     },
 
+    // Returns the format (string) of a given numeric string
+    // Note: purposively more inclusive than answer-types' predicate.forms
+    // That is, it is not necessarily true that interpreted input are numeric
+    getNumericFormat: function(text) {
+        text = $.trim(text);
+        text = text.replace(/\u2212/, "-").replace(/([+-])\s+/g, "$1");
+        if (text.match(/^[+-]?\d+$/)) {
+            return "integer";
+        } else if (text.match(/^[+-]?\d+\s+\d+\s*\/\s*\d+$/)) {
+            return "mixed";
+        }
+        var fraction = text.match(/^[+-]?(\d+)\s*\/\s*(\d+)$/);
+        if (fraction) {
+            return parseFloat(fraction[1]) > parseFloat(fraction[2]) ?
+                    "improper" : "proper";
+        } else if (text.replace(/[,. ]/g, "").match(/^\d+$/)) {
+            return "decimal";
+        } else if (text.match(/(pi?|\u03c0|t(?:au)?|\u03c4|pau)/)) {
+            return "pi";
+        } else {
+            return null;
+        }
+    },
+
+
+    // Returns a string of the number in a specified format
+    toNumericString: function(number, format) {
+        if (number == null) {
+            return "";
+        } else if (number === 0) {
+            return "0"; // otherwise it might end up as 0% or 0pi
+        }
+
+        if (format === "percent") {
+            return number * 100 + "%";
+        }
+
+        if (format === "pi") {
+            var fraction = knumber.toFraction(number / Math.PI);
+            var numerator = Math.abs(fraction[0]), denominator = fraction[1];
+            if (knumber.isInteger(numerator)) {
+                var sign = number < 0 ? "-" : "";
+                var pi = "\u03C0";
+                return sign + (numerator === 1 ? "" : numerator) + pi +
+                    (denominator === 1 ? "" : "/" + denominator);
+            }
+        }
+
+        if (_(["proper", "improper", "mixed", "fraction"]).contains(format)) {
+            var fraction = knumber.toFraction(number);
+            var numerator = Math.abs(fraction[0]), denominator = fraction[1];
+            var sign = number < 0 ? "-" : "";
+            if (denominator === 1) {
+                return sign + numerator; // for integers, irrational, d > 1000
+            } else if (format === "mixed") {
+                var modulus = numerator % denominator;
+                var integer = (numerator - modulus) / denominator;
+                return sign + (integer ? integer + " " : "") +
+                        modulus + "/" + denominator;
+            } // otherwise proper, improper, or fraction
+            return sign + numerator + "/" + denominator;
+        }
+
+        // otherwise (decimal, float, long long)
+        return String(number);
+    },
+
     // Shuffle an array using a Fischer-Yates shuffle
     // If count is passed, returns an random sublist of that size
     shuffle: function(array, count) {
@@ -588,10 +665,83 @@ $.extend(KhanUtil, {
     RED: "#DF0030",
     GRAY: "gray",
     BLACK: "black",
+    LIGHT_BLUE: "#9AB8ED",
+    LIGHT_ORANGE: "#EDD19B",
+    LIGHT_PINK: "#ED9BD3",
+    LIGHT_GREEN: "#9BEDCE",
+    LIGHT_PURPLE: "#DA9BED",
+    LIGHT_RED: "#ED9AAC",
+    LIGHT_GRAY: "#ED9B9B",
+    LIGHT_BLACK: "#ED9B9B",
+    GRAY10: "#D6D6D6",
+    GRAY20: "#CDCDCD",
+    GRAY30: "#B3B3B3",
+    GRAY40: "#9A9A9A",
+    GRAY50: "#808080",
+    GRAY60: "#666666",
+    GRAY70: "#4D4D4D",
+    GRAY80: "#333333",
+    GRAY90: "#1A1A1A",
+    BLUE_A: "#C7E9F1",
+    BLUE_B: "#9CDCEB",
+    BLUE_C: "#58C4DD",
+    BLUE_D: "#29ABCA",
+    BLUE_E: "#1C758A",
+    TEAL_A: "#ACEAD7",
+    TEAL_B: "#76DDC0",
+    TEAL_C: "#5CD0B3",
+    TEAL_D: "#55C1A7",
+    TEAL_E: "#49A88F",
+    GREEN_A: "#C9E2AE",
+    GREEN_B: "#A6CF8C",
+    GREEN_C: "#83C167",
+    GREEN_D: "#77B05D",
+    GREEN_E: "#699C52",
+    GOLD_A: "#F7C797",
+    GOLD_B: "#F9B775",
+    GOLD_C: "#F0AC5F",
+    GOLD_D: "#E1A158",
+    GOLD_E: "#C78D46",
+    RED_A: "#F7A1A3",
+    RED_B: "#FF8080",
+    RED_C: "#FC6255",
+    RED_D: "#E65A4C",
+    RED_E: "#CF5044",
+    MAROON_A: "#ECABC1",
+    MAROON_B: "#EC92AB",
+    MAROON_C: "#C55F73",
+    MAROON_D: "#A24D61",
+    MAROON_E: "#94424F",
+    PURPLE_A: "#CAA3E8",
+    PURPLE_B: "#B189C6",
+    PURPLE_C: "#9A72AC",
+    PURPLE_D: "#715582",
+    PURPLE_E: "#644172",
+    MINT_A: "#F5F9E8",
+    MINT_B: "#EDF2DF",
+    MINT_C: "#E0E5CC",
+    GRAY_A: "#FDFDFD",
+    GRAY_B: "#F7F7F7",
+    GRAY_C: "#EEEEEE",
+    GRAY_D: "#DDDDDD",
+    GRAY_E: "#CCCCCC",
+    GRAY_F: "#AAAAAA",
+    GRAY_G: "#999999",
+    GRAY_H: "#555555",
+    GRAY_I: "#333333",
+    KA_BLUE: "#314453",
+    KA_GREEN: "#639B24",
     // Don't actually use _BACKGROUND! Make things transparent instead. The
     // background color used in exercises is subject to change at the whim
     // of fickle designers.
     _BACKGROUND: "#FDFDFD"  // TODO(eater): Get rid of this altogether.
+});
+
+// For consistent coloring throughout Perseus
+$.extend(KhanUtil, {
+    INTERACTING: KhanUtil.ORANGE,
+    INTERACTIVE: KhanUtil.ORANGE,
+    DYNAMIC: KhanUtil.BLUE
 });
 
 })();
