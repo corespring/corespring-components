4709ff474e84b5a433d31c6abc798421e14918dd
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2015-08-12 16:18:18 -0600

    CO-275: Update column header display on Matrix

--- components/corespring/match/src/client/render.js
+++ components/corespring/match/src/client/render.js
@@ -208,7 +208,6 @@ var main = [
           _.forEach(columns, function(col, index){
             col.cssClass = index === 0 ? 'question-header' : 'answer-header';
             var labelWithoutTags = removeUnexpectedTags(col.labelHtml);
-              console.log('d', col.labelHtml);
             col.labelHtml = (labelWithoutTags==="Custom header" || labelWithoutTags==="Column 1" || labelWithoutTags==="Column 2" )?'':labelWithoutTags;
           });
 
