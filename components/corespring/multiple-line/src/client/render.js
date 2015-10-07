925be705d8123eeb51ba9e9733775be2e2c4d267
Javier Camacho <javier.camacho@edify.cr>
2015-10-06 21:25:10 -0600

    CO-326: Update multiple line component to render the graph

--- components/corespring/multiple-line/src/client/render.js
+++ components/corespring/multiple-line/src/client/render.js
@@ -13,15 +13,81 @@ var main = ['$compile', '$rootScope', '$timeout', "LineUtils",
 
     function link(scope, element, attrs) {
 
+      // set intitial state for jsxgraph directive
+      var createGraphAttributes = function(config, graphCallback) {
+
+        function getModelValue(property, defaultValue, fallbackValue) {
+          if (typeof property !== 'undefined' && property !== null) {
+            return property;
+          } else {
+            if (typeof fallbackValue !== 'undefined' && property !== null) {
+              return fallbackValue;
+            } else {
+              return defaultValue;
+            }
+          }
+        }
+
+        return {
+          "jsx-graph": "",
+          "graph-callback": graphCallback || "graphCallback",
+          "interaction-callback": "interactionCallback",
+          maxPoints: scope.config.lines.length * 2,
+          domainLabel: config.domainLabel,
+          domainMin: parseFloat(getModelValue(config.domainMin, -10, config.domain * -1), 10),
+          domainMax: parseFloat(getModelValue(config.domainMax, 10, config.domain), 10),
+          domainStepValue: parseFloat(getModelValue(config.domainStepValue)),
+          domainLabelFrequency: parseFloat(getModelValue(config.domainLabelFrequency, 1, config.tickLabelFrequency), 10),
+          domainGraphPadding: parseInt(getModelValue(config.domainGraphPadding, 25), 10),
+          rangeLabel: config.rangeLabel,
+          rangeMin: parseFloat(getModelValue(config.rangeMin, -10, config.range * -1)),
+          rangeMax: parseFloat(getModelValue(config.rangeMax, 10, config.range * 1)),
+          rangeStepValue: parseFloat(getModelValue(config.rangeStepValue)),
+          rangeLabelFrequency: parseFloat(getModelValue(config.rangeLabelFrequency, 1, config.tickLabelFrequency, 10)),
+          rangeGraphPadding: parseInt(getModelValue(config.rangeGraphPadding, 25), 10),
+          showLabels: !_.isUndefined(config.showLabels) ? config.showLabels : true,
+          showCoordinates: !_.isUndefined(config.showCoordinates) ? config.showCoordinates : true,
+          showPoints: !_.isUndefined(config.showPoints) ? config.showPoints : true,
+          pointLabels: !!config.showInputs ? "letters" : "none"
+        };
+      };
+
+      scope.interactionCallback = function(params) {
+      };
+
       scope.containerBridge = {
 
         setDataAndSession: function(dataAndSession) {
 
+          var config = dataAndSession.data.model.config || {};
+          scope.config = _.defaults(config, {
+            showFeedback: true
+          });
+
+          var containerWidth, containerHeight;
+          var graphContainer = element.find('.graph-container');
+          if (config.graphWidth && config.graphHeight) {
+            containerWidth = parseInt(config.graphWidth, 10);
+            containerHeight = parseInt(config.graphHeight, 10);
+          } else {
+            containerHeight = containerWidth = graphContainer.width();
+          }
+
+          var graphAttrs = createGraphAttributes(config);
+          scope.showInputs = config.showInputs;
+
+          graphContainer.attr(graphAttrs);
+          graphContainer.css({
+            width: containerWidth,
+            height: containerHeight
+          });
+          scope.containerWidth = containerWidth;
+          scope.containerHeight = containerHeight;
+          $compile(graphContainer)(scope);
         },
 
         getSession: function() {
           return {
-            answers: scope.points
           };
         },
 
@@ -55,9 +121,9 @@ var main = ['$compile', '$rootScope', '$timeout', "LineUtils",
     function template() {
       return [
         "<div class='line-interaction-view'>",
-        "  Multiple line component...<br />",
-        "  Multiple line component...<br />",
-        "  Multiple line component...",
+        "  <div class='graph-interaction'>",
+        "    <div id='graph-container' class='row-fluid graph-container'></div>",
+        "  </div>",
         "</div>"
       ].join("");
     }
