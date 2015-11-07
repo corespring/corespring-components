92560053a8be0bbc14e022616d20a730b446f892
Arturo Campos <arturo.campos@edify.cr>
2015-11-07 03:03:43 -0600

    CO-363, CO-372, CO-376, CO-377: Adapting model to a simpler format, similar to Amplify's one

--- components/corespring/select-text/src/client/render.js
+++ components/corespring/select-text/src/client/render.js
@@ -1,8 +1,7 @@
 var main = [
   '$sce',
-  '$timeout',
   'CsUndoModel',
-  function($sce, $timeout, CsUndoModel) {
+  function($sce, CsUndoModel) {
 
     var link = function(scope, element, attrs) {
 
@@ -11,10 +10,6 @@ var main = [
       scope.undoModel = new CsUndoModel();
       scope.undoModel.setGetState(getState);
       scope.undoModel.setRevertState(revertState);
-
-      var blastOptions = {
-        customClass: 'token'
-      };
 
       var $theContent = null;
 
@@ -32,60 +27,38 @@ var main = [
         }, obj);
       };
 
-      var classifyTokens = function(collection, tokenClass) {
-        if (collection.length > 0) {
-          for (var i = collection.length - 1; i >= 0; i--) {
-            var $matches = $theContent.find('.token:contains("' + collection[i].data + '"):not(.' + tokenClass + ')');
-            if ($matches.length === 1) {
-              $matches.addClass(tokenClass);
-            } else if ($matches.length > 1) {
-              if (collection[i].index) {
-                for (var j = $matches.length - 1; j >= 0; j--) {
-                  if ($theContent.find('.token').index($matches[j]) === collection[i].index) {
-                    $($matches[j]).addClass(tokenClass);
-                    break;
-                  }
-                }
-              } else {
-                $($matches[0]).addClass(tokenClass);
-              }
+      var classifyTokens = function(choices, tokenClass) {
+        if (choices.length > 0) {
+          for (var i = choices.length - 1; i >= 0; i--) {
+            var $match = $theContent.find('.cs-token:eq("' + choices[i] + '"):not(.' + tokenClass + ')');
+            if ($match.length === 1) {
+              $match.addClass(tokenClass);
             }
           }
         }
       };
 
-      var deleteItemFromCollection = function(collection, text, index) {
-        _.remove(collection, function(item) {
-          return item.data === text || item.index === index;
-        });
-      };
-
       var bindTokenEvents = function() {
-        $theContent.off('click', '.token');
-        $theContent.on('click', '.token', function() {
+        $theContent.off('click', '.cs-token');
+        $theContent.on('click', '.cs-token', function() {
           var $token = $(this);
-          var index = $theContent.find('.token').index($token);
-          if (scope.model.config.availability === "specific") {
-            if ($token.hasClass('choice') && !$token.hasClass('selected')) {
-              $token.addClass('selected');
-              scope.userChoices.push({
-                data: $token.text(),
-                index: index
-              });
-            } else if ($token.hasClass('choice') && $token.hasClass('selected')) {
-              $token.removeClass('selected');
-              deleteItemFromCollection(scope.userChoices, $token.text(), index);
-            }
-          } else {
-            $token.toggleClass('selected');
-            if ($token.hasClass('selected')) {
-              // Adds a new choice
-              scope.userChoices.push({
-                data: $token.text(),
-                index: index
-              });
+          var index = $theContent.find('.cs-token').index($token);
+          if (scope.editable) {
+            if (scope.model.config.availability === "specific") {
+              if ($token.hasClass('choice') && !$token.hasClass('selected')) {
+                $token.addClass('selected');
+                scope.userChoices.push(index);
+              } else if ($token.hasClass('choice') && $token.hasClass('selected')) {
+                $token.removeClass('selected');
+                _.remove(scope.userChoices, function(val) {return val !== index;});
+              }
             } else {
-              deleteItemFromCollection(scope.userChoices, $token.text(), index);
+              $token.toggleClass('selected');
+              if ($token.hasClass('selected')) {
+                scope.userChoices.push(index);
+              } else {
+                _.remove(scope.userChoices, function(val) {return val !== index;});
+              }
             }
           }
         });
@@ -97,17 +70,9 @@ var main = [
         scope.userChoices = [];
         $theContent = element.find('.select-text-content');
         bindTokenEvents();
-        $timeout(function() {
-          blastOptions.delimiter = getNestedProperty(scope, 'model.config.selectionUnit') ? scope.model.config.selectionUnit : 'word';
-          // Removes any existing tokens
-          $theContent.blast(false);
-          // Tokenize the content
-          $theContent.blast(blastOptions);
-          // Render existing choices
-          if (scope.model.config.availability === "specific" && getNestedProperty(scope, 'model.choices')) {
-            classifyTokens(scope.model.choices, "choice");
-          }
-        }, 100);
+        if (scope.model.config.availability === "specific" && getNestedProperty(scope, 'model.choices')) {
+          classifyTokens(scope.model.choices, "choice");
+        }
       }
 
       function getSession() {
@@ -127,19 +92,15 @@ var main = [
       function reset() {
         scope.feedback = undefined;
         scope.correctClass = undefined;
+        scope.userChoices = [];
+        $theContent.find('.selected').removeClass('selected');
       }
 
       function isAnswerEmpty() {
         return _.isEmpty(getSession().answers);
       }
 
-      function answerChangedHandler(callback) {
-        scope.$watch("selectedTokens", function(newValue, oldValue) {
-          if (newValue !== oldValue) {
-            callback();
-          }
-        }, true);
-      }
+      function answerChangedHandler() {}
 
       function editable(e) {
         scope.editable = e;
@@ -171,7 +132,7 @@ var main = [
         '    <span cs-undo-button-with-model></span>',
         '    <span cs-start-over-button-with-model></span>',
         '  </div>',
-        '  <div class="select-text-content" ng-class="{specific: model.config.availability === \'specific\'}" ng-bind-html-unsafe="model.config.xhtml"></div>',
+        '  <div class="select-text-content" ng-class="{specific: model.config.availability === \'specific\', blocked: !editable}" ng-bind-html-unsafe="model.config.passage"></div>',
         '</div>'
       ].join("\n")
     };
