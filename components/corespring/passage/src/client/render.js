b963b54c4d8fab641b3a3c58090f7e8825104f7c
Ben Burton <benjamin.joseph.burton@gmail.com>
2015-12-17 13:46:34 -0500

    Render passages

---
+++ components/corespring/passage/src/client/render.js
@@ -0,0 +1,46 @@
+var main = [
+  '$sce',
+  function($sce) {
+
+    return {
+      scope: {},
+      restrict: 'AE',
+      replace: true,
+      link: function($scope, $element, $attrs) {
+
+        function setSrc(passageId) {
+          $scope.passageRoute = $sce.trustAsResourceUrl('/api/v2/passages/' + passageId);
+        }
+
+        var addEmptyFunctions = function(obj, fns) {
+          _.each(fns, function(fn) {
+            obj[fn] = function() {
+            };
+          });
+        };
+
+        $scope.containerBridge = {
+
+          setDataAndSession: function(dataAndSession) {
+            console.log('this is being triggered.');
+            $scope.displayed = dataAndSession.data.model.config.displayed !== false;
+            setSrc(dataAndSession.data.id);
+          }
+
+        };
+
+        addEmptyFunctions($scope.containerBridge, ['setResponse', 'setMode', 'reset', 'answerChangedHandler', 'editable', 'getSession', 'isAnswerEmpty']);
+
+        $scope.$emit('registerComponent', $attrs.id, $scope.containerBridge);
+      },
+      template: [
+        '<div class="passage-frame">',
+        '  <iframe ng-src="{{passageRoute}}" ng-if="passageRoute && displayed"></iframe>',
+        '</div>'
+      ].join("\n")
+    };
+  }
+];
+
+exports.framework = 'angular';
+exports.directive = main;
