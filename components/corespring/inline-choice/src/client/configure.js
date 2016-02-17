c4be1216d44ff95862b60b4088dbaf09b32b4975
Ben Burton <benjamin.joseph.burton@gmail.com>
2016-02-17 15:39:26 -0500

    Rename short answer dd to inline choice

--- components/corespring/inline-choice/src/client/configure.js
+++ components/corespring/inline-choice/src/client/configure.js
@@ -10,7 +10,7 @@ var main = [
       '  <div class="container-fluid">',
       '    <div class="row">',
       '      <div class="col-xs-12">',
-      '        <p>In Short Answer &mdash; Drop Down, students select the best response from a list of options ',
+      '        <p>In Inline Choice, students select the best response from a list of options ',
       '           presented. This interaction type may be embedded inline with other content such as text.',
       '        </p>',
       '      </div>',
