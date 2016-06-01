c851dc107cf04ffda957cba7112de9aa44adca99
Ben Burton <benjamin.joseph.burton@gmail.com>
2016-06-01 10:52:55 -0400

    Change show/hide messaging

--- components/corespring/widgets/src/client/correct-answer-toggle.js
+++ components/corespring/widgets/src/client/correct-answer-toggle.js
@@ -1,23 +1,34 @@
 var def = function() {
+  var defaults = {
+    showMessage: 'Show Correct Answer',
+    hideMessage: 'Show My Answer'
+  };
+
   return {
     restrict: "AE",
     replace: true,
     scope: {
       'visible': '=',
-      'toggle' : '='
+      'toggle' : '=',
+      'showMessage': '@',
+      'hideMessage': '@'
     },
     link: function($scope) {
+      var showMessage = $scope.showMessage ? $scope.showMessage : defaults.showMessage;
+      var hideMessage = $scope.hideMessage ? $scope.hideMessage : defaults.hideMessage;
+
       $scope.changeToggle = function() {
         $scope.toggle = !$scope.toggle;
-        $scope.$emit('correctAnswerToggle', {open: $scope.toggle});
-        $scope.$broadcast('correctAnswerToggle', {open: $scope.toggle});
+        $scope.message = ($scope.toggle ? hideMessage : showMessage);
       };
+      
+      $scope.message = showMessage;
     },
     template: [
       '<div class="button-row btn-group-md text-center {{class}}">',
       '  <div class="show-correct" ng-if="visible" ng-click="changeToggle()">',
       '    <svg-icon category="showHide" key="correct-response" open="{{toggle}}"></svg-icon>',
-      '    <span>{{toggle ? \'Hide\' : \'Show\'}} Correct Answer</span>',
+      '    <span>{{message}}</span>',
       '  </div>',
       '</div>'
     ].join('')
