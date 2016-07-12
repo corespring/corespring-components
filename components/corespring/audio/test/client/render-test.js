076f3c0ae042df2b44bdb48ba404d62914ac8e84
rbokel <rbokel.github@qlod.org>
2016-07-12 18:37:43 +0200

    fix tests

--- components/corespring/audio/test/client/render-test.js
+++ components/corespring/audio/test/client/render-test.js
@@ -1,6 +1,6 @@
 describe('corespring-audio-render', function() {
 
-  var element, scope, rootScope, container, testModel;
+  var element, scope, rootScope, container, sce, testModel;
 
   var MockComponentRegister = function() {
     this.elements = {};
@@ -26,7 +26,7 @@ describe('corespring-audio-render', function() {
     });
   });
 
-  beforeEach(inject(function($compile, $rootScope) {
+  beforeEach(inject(function($compile, $rootScope, $sce) {
     container = new MockComponentRegister();
 
     $rootScope.$on('registerComponent', function(event, id, obj) {
@@ -37,6 +37,7 @@ describe('corespring-audio-render', function() {
     element = $compile("<corespring-audio-render id='1'></corespring-audio-render>")($rootScope.$new());
     scope = element.scope().$$childHead;
     rootScope = $rootScope;
+    sce = $sce;
   }));
 
   it('constructs', function() {
@@ -55,17 +56,49 @@ describe('corespring-audio-render', function() {
     expect(scope).toBeTruthy();
   });
 
-  it('sets model', function() {
-    container.elements['1'].setDataAndSession(testModel);
-    expect(scope.config.ui).toBe('fullControls');
-    expect(scope.config.playButtonLabel).toBe('>');
-    expect(scope.config.pauseButtonLabel).toBe('||');
-    expect(scope.config.fileName).toEqual('test.mp3');
-    expect(scope.sources.length).toEqual(1);
-    expect(scope.sources[0].type).toEqual('audio/mp3');
-    expect(scope.sources[0].url).toEqual('test.mp3');
-    expect(typeof scope.sources[0].trustedUrl).toEqual('object');
-    expect(scope.sources[0].trustedUrl.toString()).toEqual('test.mp3');
+  describe('setDataAndSession', function() {
+
+    beforeEach(function () {
+      container.elements['1'].setDataAndSession(testModel);
+    });
+
+    describe('config', function(){
+      it('sets ui', function () {
+        expect(scope.config.ui).toBe('fullControls');
+      });
+
+      it('sets playButtonLabel', function () {
+        expect(scope.config.playButtonLabel).toBe('>');
+      });
+
+      it('sets pauseButtonLabel', function () {
+        expect(scope.config.pauseButtonLabel).toBe('||');
+      });
+
+      it('sets fileName', function () {
+        expect(scope.config.fileName).toEqual('test.mp3');
+      });
+    });
+
+    describe('sources', function(){
+      it('adds two sources', function () {
+        expect(scope.sources.length).toEqual(2);
+      });
+
+      it('sets fileName as source with mime audio/mp3', function () {
+        expect(scope.sources[0].type).toEqual('audio/mp3');
+        expect(scope.sources[0].url).toEqual('test.mp3');
+        expect(typeof scope.sources[0].trustedUrl).toEqual('object');
+        expect(sce.getTrustedUrl(scope.sources[0].trustedUrl)).toEqual('test.mp3');
+      });
+
+      it('sets fileName as source with mime audio/mpeg', function () {
+        expect(scope.sources[1].type).toEqual('audio/mpeg');
+        expect(scope.sources[1].url).toEqual('test.mp3');
+        expect(typeof scope.sources[1].trustedUrl).toEqual('object');
+        expect(sce.getTrustedUrl(scope.sources[1].trustedUrl)).toEqual('test.mp3');
+      });
+    });
   });
 
 });
