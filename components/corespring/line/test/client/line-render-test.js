4494a16dd0605be28d2d162a0b744eb0e1024887
rbokel <rbokel.github@qlod.org>
2016-08-03 15:55:06 +0200

    merge

--- components/corespring/line/test/client/line-render-test.js
+++ components/corespring/line/test/client/line-render-test.js
@@ -207,4 +207,33 @@ describe('corespring:line:render', function() {
     });
 
   });
+
+  describe('order of setMode/setResponse', function() {
+    var response;
+
+    beforeEach(function() {
+      container.elements['1'].setDataAndSession(_.cloneDeep(testModel));
+      rootScope.$digest();
+
+      response = {correctness: 'incorrect', feedback: 'not good'};
+    });
+
+    function assertFeedback() {
+      rootScope.$digest();
+      expect(scope.feedback).toEqual('not good');
+    }
+
+    it('should work when setMode is called before setResponse', function() {
+      container.elements['1'].setMode('evaluate');
+      container.elements['1'].setResponse(response);
+      assertFeedback();
+    });
+
+    it('should work when setMode is called after setResponse', function() {
+      container.elements['1'].setResponse(response);
+      container.elements['1'].setMode('evaluate');
+      assertFeedback();
+    });
+  });
+
 });
